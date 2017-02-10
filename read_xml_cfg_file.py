29f2fe61fa1d4c17f1a796f40d5f53aa4db6c85d
JP <peacock.jared@gmail.com>
2017-02-10 13:13:05 -0800

    making read xml cleaner

--- read_xml_cfg_file.py
+++ read_xml_cfg_file.py
@@ -13,52 +13,88 @@ import mtpy.core.mt_xml as mtxml
 #        for key in kwargs.keys():
 #            setattr(self, key, kwargs[key])
 
-def make_attr_str(child):
-    tag = child.tag
-    attributes = child.attrib
-    for key in attributes.keys():
-        tag += '({0}={1})'.format(key, attributes[key])
-        
-    return tag
+def get_info_from_element(element):
+    return_obj = mtxml.Dummy()
+    return_obj._name = element.tag
+    try:
+        return_obj._text = element.text.strip()
+    except AttributeError:
+        return_obj._text = None
+    return_obj._attr = element.attrib
+    
+    return return_obj
+    
+def get_attr_name(parent, attr_name):
+    if hasattr(parent, attr_name):
+        print 'parent already has attribute {0}'.format(attr_name)
+        new_attr_name = '{0}_{1:02}'.format(attr_name, 1)
+        if hasattr(parent, new_attr_name):
+            count = 2
+            while hasattr(parent, '{0}_{1:02}'.format(attr_name, count)):
+                new_attr_name = '{0}_{1:02}'.format(attr_name, count)
+                count += 1
+    else:
+        new_attr_name = attr_name
+    
+    return new_attr_name
 
 xml_fn = r"C:\Users\jpeacock\Documents\PyScripts\ORL09bc_J9.xml"
 
-et_xml = ET.parse(xml_fn)
-
-root = et_xml.getroot()
-
 e2xml = mtxml.EDI_to_XML()
 
+et_xml = ET.parse(xml_fn)
 
-for child_00 in root.getchildren():
-    attr_00 = make_attr_str(child_00)
+
+def read_element(element):
+    """
+    read a given element and return something useful
+    """
     
-    if len(child_00.getchildren()) > 0:
-        value_00 = mtxml.Dummy(**{'_name':attr_00})
-        for child_01 in child_00.getchildren():
-            attr_01 = make_attr_str(child_01)
-            if len(child_01.getchildren()) > 0:
-                value_01 = mtxml.Dummy()
-                for child_02 in child_01.getchildren():
-                    attr_02 = make_attr_str(child_02)
-                    value_02 = child_02.text
-                    setattr(value_01, attr_02, value_02)
-                setattr(value_01, '_name', attr_01)
+    child = get_info_from_element(element)
+    
+    children = element.getchildren()
+    if len(children) > 0:
+        for child_00 in children:
+            attr_name = get_attr_name(child, child_00.tag)
+            setattr(child, attr_name, get_info_from_element(child_00))
+            
+            children_01 = child_00.getchildren()
+            if len(children_01) > 0:
+                for child_01 in children_01:
+                    attr_01_name = get_attr_name(getattr(child, attr_name),
+                                                 child_01.tag)
+                                                 
+                    setattr(getattr(child, child_00.tag), attr_01_name,
+                            get_info_from_element(child_01))
+                    
 
+
+                
             else:
-                value_01 = child_01.text
-            setattr(value_00, attr_01, value_01)
-        
-        setattr(value_00, '_name', attr_00)
-        
+                pass
+                    
+
     else:
-        value_00 = child_00.text
-    if '(' and ')' in attr_00:
-        setattr(e2xml.cfg_obj, child_00.tag, value_00)
-    else:
-        setattr(e2xml.cfg_obj, attr_00, value_00)
-    
-e2xml.write_xml(xml_fn=r"C:\Users\jpeacock\Documents\PyScripts\Test_read_xml.xml")
-    
-   
+        pass
+        
+    return child
+
+# read description
+#description = et_xml.find('Description')
+#k = read_element(description)
+
+ext_url = et_xml.find('FieldNotes')
+k = read_element(ext_url)
+
+
+#root = et_xml.getroot()
+#
+#
+#
+#
+
+#    
+##e2xml.write_xml(xml_fn=r"C:\Users\jpeacock\Documents\PyScripts\Test_read_xml.xml")
+#    
+#   
 
