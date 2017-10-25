fd0b6a6eb6ef74669063d79bce4b8b1b9ba04c8f
JP <peacock.jared@gmail.com>
2017-10-25 16:16:06 -0700

    write netcdf file

--- tongario_plot.py
+++ tongario_plot.py
@@ -9,15 +9,27 @@ import os
 import mtpy.core.mt as mt
 import numpy as np
 import mtpy.analysis.pt as mtpt
+import scipy.signal as signal
+
+import matplotlib.pyplot as plt
 
 
 dir_path_01 = r"c:\Users\jrpeacock\Documents\Test_Data\Tongario\original"
 dir_path_02 = r"c:\Users\jrpeacock\Documents\Test_Data\Tongario\repeat"
 
-rpt_arr = np.zeros(5, dtype=[('station', 'S10')])
+station_num = [3, 4, 13, 24, 30, 70, 62, 1, 2, 4, 8, 9, 12, 14, 27, 28, 25]
+noiseless = np.array([0, 3, 5, 6, 8, 10, 11, 13, 14, 15])
+rpt_arr = np.zeros(len(station_num),
+                   dtype=[('station', 'S10'),
+                             ('frequency', (np.float, (40,))),
+                             ('phimax', (np.float, (40))),
+                             ('phimin', (np.float, (40,))),
+                             ('phimax_err', (np.float, (40,))),
+                             ('phimin_err', (np.float, (40,)))])
 
-repeat_list = ['station,repeatability(%),repeatability_err(%)']
-for station in ([3, 4, 13, 24, 30]):
+z_err = np.zeros((40, 2, 2), dtype=np.complex)
+
+for ii, station in enumerate(station_num):
     fn_01 = os.path.join(dir_path_01, 'TNG-0{0:02}.edi'.format(station))
     fn_02 = os.path.join(dir_path_02, 'TNG-3{0:02}.edi'.format(station))
     
@@ -27,27 +39,57 @@ for station in ([3, 4, 13, 24, 30]):
     mt_02 = mt.MT()
     mt_02.read_mt_file(fn_02)
     
-    # get frequencies where no change is expected
-    no_change = np.where(1./mt_01.Z.freq < 2)
+    z_err += mt_01.Z.z_err
+    z_err += mt_02.Z.z_err
     
-    # estimate a static shift in each mode
-    ss_x = 1./np.sqrt(np.median(np.abs(mt_02.Z.z[no_change, 0, 1]/mt_01.Z.z[no_change, 0, 1])))
-    ss_y = 1./np.sqrt(np.median(np.abs(mt_02.Z.z[no_change, 1, 0]/mt_01.Z.z[no_change, 1, 0])))
-    
-    mt_02.Z = mt_02.remove_static_shift(ss_x=ss_x, ss_y=ss_y)
     rpt = mtpt.ResidualPhaseTensor(mt_01.pt, mt_02.pt)
+    rpt_arr['frequency'][ii] = mt_01.Z.freq
+    rpt_arr['station'][ii] = mt_01.station
+    rpt_arr['phimax'][ii][:] = rpt.residual_pt.phimax
+    rpt_arr['phimin'][ii][:] = rpt.residual_pt.phimin
+    rpt_arr['phimax_err'][ii][:] = rpt.residual_pt.phimax_err
+    rpt_arr['phimin_err'][ii][:] = rpt.residual_pt.phimin_err
     
-    repeat = np.median(np.abs(rpt.residual_pt.pt[no_change]))*100
-    repeat_err = np.median(np.abs(rpt.residual_pt.pt_err[no_change]))*100
+## make an error phase tensor
+#z_err_object = mt.MTz.Z(z_array=z_err/10., freq=rpt_arr['frequency'][0])
+#err_pt = mtpt.PhaseTensor(z_object=z_err_object)
+    
+fig = plt.figure(1)
+fig.subplots_adjust(left=.06, bottom=.06, right=.98, top=.95)
+line_list = []
+label_list = []
+for ii, r_arr in enumerate(rpt_arr[0:16]):
+    ax = fig.add_subplot(16, 1, ii+1)
+    line = ax.fill_between(1./rpt_arr['frequency'][0], 
+                       np.zeros(40),
+                       signal.medfilt(np.mean(rpt_arr['phimax'][noiseless],
+                                              axis=0),
+                                      kernel_size=7),
+                        color=(.75, .75, .75),
+                        alpha=.35)
 
-    print 'TNG-0{0:02}, Repeatability is {1:.2f}% +/- {2:.2f}%'.format(station,
-                                                                repeat,
-                                                                repeat_err)
-    repeat_list.append('TNG-0{0:02},{1:.2f}%,{2:.2f}'.format(station,
-                                                              repeat,
-                                                              repeat_err))
+    line = ax.errorbar(1./r_arr['frequency'], 
+                        signal.medfilt(r_arr['phimax'], kernel_size=7),
+                        yerr=signal.medfilt(r_arr['phimax_err'], kernel_size=7),
+                        capsize=5,
+                        capthick=.5)
+    
+    line_list.append(line)
+    label_list.append(r_arr['station'])
+    ax.set_xscale('log')
+    ax.grid(which='both', color=(.5, .5, .5), ls='--')
+    ax.set_axisbelow(True)
+    ax.set_ylim(0, 90)
+    ax.set_yticks(np.arange(0, 90, 10))
+    ax.set_ylabel('Phimax', fontdict={'size':12, 'weight':'bold'})
+    ax.legend(line, [r_arr['station']], loc='upper left')
 
-with open(r"c:\Users\jrpeacock\Documents\Test_Data\Tongario\repeatability_pt_det.txt", 'w') as fid:
-    fid.write('\n'.join(repeat_list))
+#line_list.append(line)
+#label_list.append('Error')
     
-    
+#fig.legend(line_list, label_list, loc='upper center', ncol=6)
+
+ax.set_xlabel('Period (s)', fontdict={'size':12, 'weight':'bold'})
+
+
+plt.show()
---
+++ write_netcdf_file.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 15:05:19 2017
+
+@author: jrpeacock
+"""
+
+import netCDF4 
+import mtpy.modeling.modem as modem
+import mtpy.utils.gis_tools as gis_tools
+import numpy as np
+
+mfn = r"c:\Users\jrpeacock\Google Drive\LV_Geothermal\SI_Peacock_etal2016_D2.rho"
+
+model_center = (38.0, -115.0)
+model_center_ne = gis_tools.project_point_ll2utm(model_center[0], 
+                                                 model_center[1])
+
+m_obj = modem.Model()
+m_obj.read_model_file(mfn)
+
+# need to make a lat and lon list
+lat = np.zeros(m_obj.nodes_north.size)
+lon = np.zeros(m_obj.nodes_east.size)
+
+for ii, north in enumerate(model_center_ne[0]-m_obj.grid_north[:-1]):
+    lat_00, lon_00 = gis_tools.project_point_utm2ll(north, 
+                                                    model_center_ne[1],
+                                                    model_center_ne[2])
+    lat[ii] = lat_00
+    
+for ii, east in enumerate(model_center_ne[1]-m_obj.grid_east[:-1]):
+    lat_00, lon_00 = gis_tools.project_point_utm2ll(model_center_ne[0],
+                                                    east,
+                                                    model_center_ne[2])
+    lon[ii] = lon_00
+
+#nc_obj = netCDF4.Dataset(m_obj.model_fn[:-4]+'.nc', 'w', format='NETCDF4')
+nc_obj = netCDF4.Dataset(r"c:\Users\jrpeacock\test_00.nc", 'w', format='NETCDF4')
+nc_obj.setncattr('description', 'Resistivity Model from ModEM')
+
+# dimensions
+nc_obj.createDimension('latitude', lat.size)
+nc_obj.createDimension('longitude', lon.size)
+nc_obj.createDimension('depth', m_obj.grid_z.size-1)
+
+# variables
+nc_lat = nc_obj.createVariable('latitude', 'f8', ('latitude',))
+nc_lon = nc_obj.createVariable('longitude', 'f8', ('longitude',))
+nc_depth = nc_obj.createVariable('depth', 'f8', ('depth',))
+nc_res = nc_obj.createVariable('resistivity', 'f8',
+                               ('latitude', 'longitude', 'depth'))
+nc_lat[:] = lat
+nc_lon[:] = lon
+nc_depth[:] = m_obj.grid_z[:-1]
+nc_res[:] = m_obj.res_model
+
+nc_obj.close()
