8462fc341c3511788468adcef368e5ce66b77a68
JP <peacock.jared@gmail.com>
2017-12-07 16:10:09 -0800

    adding scripts

--- project_point_test.py
+++ project_point_test.py
@@ -8,8 +8,6 @@ Created on Fri Apr 14 14:47:48 2017
 from osgeo import ogr
 from osgeo import osr
 
-import osr
-
 def utm_getZone(longitude):
     return (int(1+(longitude+180.0)/6.0))
 
@@ -20,31 +18,42 @@ def utm_isNorthern(latitude):
         return 1;
 
 
-   
-lat = 38.0; lon  = -50.0
+lat, lon = (38.839336, -122.833197)
 utm_zone = utm_getZone(lon); #print "utm_zone: "; print utm_zone
 is_northern = utm_isNorthern(lat); #print "is_northern: "; print
 
-   
-## set utm coordinate system
+ll_cs = osr.SpatialReference()
+ll_cs.SetWellKnownGeogCS('WGS84')
+
 utm_cs = osr.SpatialReference()
-utm_cs.SetWellKnownGeogCS('WGS84')
-utm_cs.SetUTM(utm_zone,is_northern);
-   
-## set wgs84 coordinate system
-wgs84_cs = utm_cs.CloneGeogCS(); #print "\nwgs84_cs:"; print
-wgs84_cs.ExportToPrettyWkt()
-   
-## set the transform wgs84_to_utm and do the transform
-transform_WGS84_To_UTM = osr.CoordinateTransformation(wgs84_cs,utm_cs)
-utm_points = list(transform_WGS84_To_UTM.TransformPoint(lon,lat));
-print "utm_points: "; print utm_points
-   
-## set the transform utm_to_wgs84 and do the transform
-transform_UTM_To_WGS84 = osr.CoordinateTransformation(utm_cs,wgs84_cs)
-latlon_points = list(transform_UTM_To_WGS84.TransformPoint(utm_points[0],
-                                                           utm_points[1]))
-print "latlon_points: "; print latlon_points
+utm_cs.ImportFromEPSG(26742)
+
+transform = osr.CoordinateTransformation(ll_cs, utm_cs)
+print  transform.TransformPoint(lon, lat)
+
+#point = ogr.CreateGeometryFromWkt("POINT (1120351.57 741921.42)")
+#point.Transform(transform)
+
+#print point.ExportToWkt()
+### set utm coordinate system
+#ll_cs = osr.SpatialReference()
+#ll_cs.SetWellKnownGeogCS('WGS84')
+#utm_cs.SetUTM(utm_zone,is_northern);
+#   
+### set wgs84 coordinate system
+#wgs84_cs = utm_cs.CloneGeogCS(); #print "\nwgs84_cs:"; print
+#wgs84_cs.ExportToPrettyWkt()
+#   
+### set the transform wgs84_to_utm and do the transform
+#transform_WGS84_To_UTM = osr.CoordinateTransformation(wgs84_cs,utm_cs)
+#utm_points = list(transform_WGS84_To_UTM.TransformPoint(lon,lat));
+#print "utm_points: "; print utm_points
+#   
+### set the transform utm_to_wgs84 and do the transform
+#transform_UTM_To_WGS84 = osr.CoordinateTransformation(utm_cs,wgs84_cs)
+#latlon_points = list(transform_UTM_To_WGS84.TransformPoint(utm_points[0],
+#                                                           utm_points[1]))
+#print "latlon_points: "; print latlon_points
 
 #source = osr.SpatialReference()
 #source.ImportFromEPSG(4326)
--- tongario_plot.py
+++ tongario_plot.py
@@ -10,6 +10,7 @@ import mtpy.core.mt as mt
 import numpy as np
 import mtpy.analysis.pt as mtpt
 import scipy.signal as signal
+import scipy.interpolate as interpolate
 
 import matplotlib.pyplot as plt
 
@@ -18,6 +19,7 @@ dir_path_01 = r"c:\Users\jrpeacock\Documents\Test_Data\Tongario\original"
 dir_path_02 = r"c:\Users\jrpeacock\Documents\Test_Data\Tongario\repeat"
 
 station_num = [3, 4, 13, 24, 30, 70, 62, 1, 2, 4, 8, 9, 12, 14, 27, 28, 25]
+plot_station_index = [0, 3, 4, 5, 6]
 noiseless = np.array([0, 3, 5, 6, 8, 10, 11, 13, 14, 15])
 rpt_arr = np.zeros(len(station_num),
                    dtype=[('station', 'S10'),
@@ -54,35 +56,62 @@ for ii, station in enumerate(station_num):
 #z_err_object = mt.MTz.Z(z_array=z_err/10., freq=rpt_arr['frequency'][0])
 #err_pt = mtpt.PhaseTensor(z_object=z_err_object)
     
-fig = plt.figure(1)
-fig.subplots_adjust(left=.06, bottom=.06, right=.98, top=.95)
+fig = plt.figure(1, [7, 8])
+fig.subplots_adjust(left=.1, bottom=.06, right=.98, top=.95, hspace=.07)
 line_list = []
 label_list = []
-for ii, r_arr in enumerate(rpt_arr[0:16]):
-    ax = fig.add_subplot(16, 1, ii+1)
-    line = ax.fill_between(1./rpt_arr['frequency'][0], 
-                       np.zeros(40),
-                       signal.medfilt(np.mean(rpt_arr['phimax'][noiseless],
-                                              axis=0),
-                                      kernel_size=7),
-                        color=(.75, .75, .75),
-                        alpha=.35)
+plot_period = 1./rpt_arr['frequency'][0]
+interp_period = np.arange(plot_period.min(), plot_period.max())
 
-    line = ax.errorbar(1./r_arr['frequency'], 
-                        signal.medfilt(r_arr['phimax'], kernel_size=7),
-                        yerr=signal.medfilt(r_arr['phimax_err'], kernel_size=7),
-                        capsize=5,
-                        capthick=.5)
+noise = signal.medfilt(np.mean(rpt_arr['phimax'][noiseless], axis=0),
+                           kernel_size=7)
+noise_func = interpolate.interp1d(plot_period, noise, kind='slinear')
+plot_noise = noise_func(interp_period)
+
+for ii, r_arr in enumerate(rpt_arr[plot_station_index]):
+    phimax = signal.medfilt(r_arr['phimax'], kernel_size=7)
+    phimax_err = signal.medfilt(r_arr['phimax_err'], kernel_size=7)
     
-    line_list.append(line)
+    pm_func = interpolate.interp1d(plot_period, phimax, kind='slinear')
+    pm_err_func = interpolate.interp1d(plot_period, phimax_err, kind='slinear')
+    
+    ax = fig.add_subplot(len(plot_station_index), 1, ii+1)
+    line1 = ax.fill_between(interp_period, 
+                           np.zeros(len(interp_period)),
+                           plot_noise,
+                           color=(.65, .65, .65),
+                           alpha=.35)
+
+    line2 = ax.errorbar(plot_period, 
+                       pm_func(plot_period),
+                       pm_err_func(plot_period),
+                       ls='None',
+                       capsize=8,
+                       capthick=1.5, 
+                       lw=1.5,
+                       color=u'#1f77b4')
+    line3 = ax.errorbar(interp_period, 
+                       pm_func(interp_period),
+                       yerr=None,
+                       capsize=8,
+                       capthick=1.5, 
+                       lw=1.5,
+                       color=u'#1f77b4')
+    
+    line_list.append(line3)
     label_list.append(r_arr['station'])
     ax.set_xscale('log')
-    ax.grid(which='both', color=(.5, .5, .5), ls='--')
+    if ii < 4:
+        ax.xaxis.set_tick_params(labelbottom='off')
+    ax.set_xlim(plot_period.min(), plot_period.max())
+    ax.grid(which='both', color=(.75, .75, .75), ls='--', alpha=.7)
+    
     ax.set_axisbelow(True)
     ax.set_ylim(0, 90)
     ax.set_yticks(np.arange(0, 90, 10))
-    ax.set_ylabel('Phimax', fontdict={'size':12, 'weight':'bold'})
-    ax.legend(line, [r_arr['station']], loc='upper left')
+    ax.set_ylabel('Phimax (deg)', fontdict={'size':12, 'weight':'bold'})
+    ax.legend(line3, [r_arr['station']], loc='upper left')
+    
 
 #line_list.append(line)
 #label_list.append('Error')
@@ -91,5 +120,7 @@ for ii, r_arr in enumerate(rpt_arr[0:16]):
 
 ax.set_xlabel('Period (s)', fontdict={'size':12, 'weight':'bold'})
 
+#fig.savefig(r"c:\Users\jrpeacock\Documents\Test_Data\Tongario\Tongario_phimax_comparison_vs_error_final.pdf",
+#            dpi=600)
 
 plt.show()
