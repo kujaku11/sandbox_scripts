edb8bace5f1bed1f32cfc459e7268f8afa405bda
JP <peacock.jared@gmail.com>
2018-06-15 15:12:17 -0700

    adding scripts

--- usgs_combine_cfg_files.py
+++ usgs_combine_cfg_files.py
@@ -14,9 +14,13 @@ import mtpy.usgs.usgs_cache as usgs_cache
 # Functions to help analyze config files
 # =============================================================================
 def check_data(database, name):
-    
-    if database.notes[0] == 'None':
+    """
+    check the columns with base name to make sure all values are equal
+    """
+    if database.notes[0] in [None, 'none', 'None']:
         database.notes[0] = ''
+    elif database.notes[0] == '':
+        pass
     elif database.notes[0][-1] != ';':
         database.notes[0] += ';'
     # check start times
@@ -27,24 +31,29 @@ def check_data(database, name):
     test = np.array([value_arr[0] == value_arr[ii] for ii in range(value_arr.size)])
     if not (test == True).sum() == test.size:
         
-        tf_dict = dict(zip(*np.unique(test, return_counts=True))) 
+        #tf_dict = dict(zip(*np.unique(test, return_counts=True))) 
         
-        if tf_dict[True] > tf_dict[False]:
+#        if tf_dict[True] > tf_dict[False]:
+        if test[0] == True and (test[1:] == False).sum() == test[1:].size:
+            database.notes[0] += ' {0} is off;'.format(name_labels[0])
+        else:
             for comp, test in zip(name_labels, test):
                 if test == False:
                     database.notes[0] += ' {0} is off;'.format(comp)
-        elif tf_dict[True] < tf_dict[False]:
-            for comp, test in zip(name_labels, test):
-                if test == True:
-                    database.notes[0] += ' {0} is off;'.format(comp)
+#        elif tf_dict[True] < tf_dict[False]:
+#            for comp, test in zip(name_labels, test):
+#                if test == True:
+#                    database.notes[0] += ' {0} is off;'.format(comp)
     return database
 
 def check_std(database, tol=.005):
     """
     check standard deviation for bad data
     """
-    if database.notes[0] == 'None':
+    if database.notes[0] in [None, 'none', 'None']:
         database.notes[0] = ''
+    elif database.notes[0] == '':
+        pass
     elif database.notes[0][-1] != ';':
         database.notes[0] += ';'
         
@@ -63,22 +72,47 @@ def check_db(database):
     convinience function to check all values in database
     """
     for key in ['_start', '_nsamples']:
-        database = check_data(database, name)
+        database = check_data(database, key)
     database = check_std(database)
+    
+    return database
 # =============================================================================
 # test the function
 # =============================================================================
-cfg_dir = r"c:\Users\jpeacock\Documents\imush\G016"
+station_dir = r"c:\Users\jpeacock\Documents\imush"
 
-cfg_fn_list = sorted([os.path.join(cfg_dir, fn) for fn in os.listdir(cfg_dir)
-                      if 'mtft24' not in fn])
+for station in os.listdir(station_dir):
+    cfg_dir = os.path.join(station_dir, station)
+    if not os.path.isdir(cfg_dir):
+        continue
+    if station in ['H020']:
+        continue
+    
+    cfg_fn_list = sorted([os.path.join(cfg_dir, fn) for fn in os.listdir(cfg_dir)
+                          if 'mtft24' not in fn and 'runs' not in fn])
+    
+    count = 0
+    for cfg_fn in cfg_fn_list:
+        cfg_dict = configfile.read_configfile(cfg_fn)
+        if count == 0:
+            cfg_db = check_db(pd.DataFrame([cfg_dict[cfg_dict.keys()[0]]]))
+            count += 1
+        else:
+            cfg_db = cfg_db.append(check_db(pd.DataFrame([cfg_dict[cfg_dict.keys()[0]]])))
+            count += 1
+    
+    cfg_db.to_csv(os.path.join(cfg_dir, '{0}_runs.csv'.format(station)),
+                  index=False)
 
-count = 0
-for cfg_fn in cfg_fn_list:
-    cfg_dict = configfile.read_configfile(cfg_fn)
-    if count == 1:
-        cfg_db = check_db(pd.DataFrame([cfg_dict[cfg_dict.keys()[0]]]))
-        
-        count += 1
-    else:
-        cfg_db.append(check_db(pd.DataFrame([cfg_dict[cfg_dict.keys()[0]]])))
+#(1) site name
+#(2) siteID
+#(3) lat
+#(4) lon
+#(5) national map elevation
+#(6) Hx azimuth
+#(7) Ex azimuth
+#(8) start date [yyyymmdd]
+#(9) Ex dipole length [m]
+#(10) Ey dipole length [m]
+#(11) wideband channels
+#(12) long period channel
