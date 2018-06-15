524e377ee0886577d78073a679205650a3e30e16
JP <peacock.jared@gmail.com>
2018-06-15 15:44:08 -0700

    adding scripts

--- usgs_combine_cfg_files.py
+++ usgs_combine_cfg_files.py
@@ -81,6 +81,7 @@ def check_db(database):
 # =============================================================================
 station_dir = r"c:\Users\jpeacock\Documents\imush"
 
+s_count = 0
 for station in os.listdir(station_dir):
     cfg_dir = os.path.join(station_dir, station)
     if not os.path.isdir(cfg_dir):
@@ -89,7 +90,8 @@ for station in os.listdir(station_dir):
         continue
     
     cfg_fn_list = sorted([os.path.join(cfg_dir, fn) for fn in os.listdir(cfg_dir)
-                          if 'mtft24' not in fn and 'runs' not in fn])
+                          if 'mtft24' not in fn and 'runs' not in fn 
+                          and fn.endswith('.cfg')])
     
     count = 0
     for cfg_fn in cfg_fn_list:
@@ -104,15 +106,61 @@ for station in os.listdir(station_dir):
     cfg_db.to_csv(os.path.join(cfg_dir, '{0}_runs.csv'.format(station)),
                   index=False)
 
-#(1) site name
-#(2) siteID
-#(3) lat
-#(4) lon
-#(5) national map elevation
-#(6) Hx azimuth
-#(7) Ex azimuth
-#(8) start date [yyyymmdd]
-#(9) Ex dipole length [m]
-#(10) Ey dipole length [m]
-#(11) wideband channels
-#(12) long period channel
+    # make a single file that summarizes
+    #(1) site name
+    #(2) siteID
+    #(3) lat
+    #(4) lon
+    #(5) national map elevation
+    #(6) Hx azimuth
+    #(7) Ex azimuth
+    #(8) start date [yyyymmdd]
+    #(9) Ex dipole length [m]
+    #(10) Ey dipole length [m]
+    #(11) wideband channels
+    #(12) long period channel
+    station_dict = pd.compat.OrderedDict()
+    station_dict['site_name'] = station
+    station_dict['siteID'] = station
+    station_dict['lat'] = cfg_db.lat.astype(np.float).mean()
+    station_dict['lon'] = cfg_db.lon.astype(np.float).mean()
+    station_dict['nm_elev'] = cfg_db.elev.astype(np.float).mean()
+    station_dict['hx_azm'] = cfg_db.hx_azm.astype(np.float).median()
+    station_dict['ex_azm'] = cfg_db.ex_azm.astype(np.float).median()
+    station_dict['start_date'] = cfg_db.start_date.min().split('T')[0].replace('-', '')
+    station_dict['ex_len'] = cfg_db.ex_len.astype(np.float).median()
+    station_dict['ey_len'] = cfg_db.ey_len.astype(np.float).median()
+    station_dict['wb'] = cfg_db.n_chan.astype(np.int).median()
+    station_dict['lp'] = 0
+    
+    #(1) site name
+    #(2) lat
+    #(3) lon
+    #(4) national map elevation
+    #(5) start date
+    #(6) end date
+    #(7) instrument type [W = wideband, L = long period]
+    #(8) quality factor from 1-5 [5 is best]
+    loc_dict = pd.compat.OrderedDict()
+    loc_dict['site_name'] = station
+    loc_dict['lat'] = cfg_db.lat.astype(np.float).mean()
+    loc_dict['lon'] = cfg_db.lon.astype(np.float).mean()
+    loc_dict['nm_elev'] = cfg_db.elev.astype(np.float).mean()
+    loc_dict['start_date'] = cfg_db.start_date.min().split('T')[0].replace('-', '')
+    loc_dict['stop_date'] = cfg_db.stop_date.max().split('T')[0].replace('-', '')
+    loc_dict['instrument'] = 'W'
+    loc_dict['quality'] = 5
+    
+    if s_count == 0:
+        s_db = pd.DataFrame([station_dict])
+        l_db = pd.DataFrame([loc_dict])
+        s_count += 1
+    else:
+        s_db = s_db.append(pd.DataFrame([station_dict]))
+        l_db = l_db.append(pd.DataFrame([loc_dict]))
+        s_count += 1 
+
+s_db.to_csv(os.path.join(station_dir, 'station_info.csv'.format(station)),
+            index=False)
+l_db.to_csv(os.path.join(station_dir, 'location_info.csv'.format(station)),
+            index=False)
