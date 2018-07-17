f055ad419460c9264e8d312ea9f625d1b2ddfa3e
JP <peacock.jared@gmail.com>
2018-07-17 13:03:18 -0700

    adding scripts

--- archive_in_science_base.py
+++ archive_in_science_base.py
@@ -5,6 +5,8 @@ Created on Thu Jul 12 12:55:59 2018
 @author: jpeacock
 """
 import os
+import sys
+from cStringIO import StringIO
 import mtpy.usgs.usgs_archive as archive
 import datetime
 
@@ -18,6 +20,19 @@ survey = 'iMUSH'
 save_dir = os.path.join(survey_dir, 'Archive')
 if not os.path.exists(save_dir):
     os.mkdir(save_dir)
+    
+# =============================================================================
+# class for capturing the output to store in a file
+# =============================================================================
+# this should capture all the print statements
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        sys.stdout = self._stdout
 # =============================================================================
 # make the survey xml file
 # =============================================================================
@@ -44,46 +59,58 @@ for station in os.listdir(survey_dir):
         zm = archive.USGSasc()
         asc_fn_list = ['{0}{1}'.format(station.upper(), ext) for ext in 
                        ['_meta.xml', '.edi', '.png']]
-        for fn_block in fn_list:
-            zm.get_z3d_db(fn_block)
-            zm.read_mtft24_cfg()
-            zm.CoordinateSystem = 'Geomagnetic North'
-            zm.SurveyID = survey
-            zm.write_asc_file(save_dir=station_save_dir,
-                              full=False, compress=True)
-            asc_fn_list.append(os.path.basename(zm._make_file_name(save_path=station_save_dir, 
-                                                  compression=True)))
-            zm.write_station_info_metadata(save_dir=station_save_dir)
+        print('--> Archiving station {0}'.format(station))
+        # capture the output to put into a log file for each station, just to
+        # be sure and capture what happened.
+        with Capturing() as output:
+            for fn_block in fn_list:
+                zm.get_z3d_db(fn_block)
+                mtft_find = zm.read_mtft24_cfg()
+                zm.CoordinateSystem = 'Geomagnetic North'
+                zm.SurveyID = survey
+                zm.write_asc_file(save_dir=station_save_dir,
+                                  full=False, compress=True)
+                asc_fn_list.append(os.path.basename(zm._make_file_name(save_path=station_save_dir, 
+                                                      compression=True)))
+                zm.write_station_info_metadata(save_dir=station_save_dir,
+                                               mtft_bool=mtft_find)
+                
+            # make a station database
+            s_cfg = archive.USGScfg()
+            s_db, csv_fn = s_cfg.combine_run_cfg(station_save_dir)
             
-        # make a station database
-        s_cfg = archive.USGScfg()
-        s_db, csv_fn = s_cfg.combine_run_cfg(station_save_dir)
+            # make xml file
+            s_xml = archive.XMLMetadata()
+            s_xml.read_config_file(survey_cfg)
+            s_xml.supplement_info = s_xml.supplement_info.replace('\\n', '\n\t\t\t')
+            
+            # location
+            s_xml.survey.east = s_db.lon.median()
+            s_xml.survey.west = s_db.lon.median()
+            s_xml.survey.north = s_db.lat.median()
+            s_xml.survey.south = s_db.lat.median()
+            
+            # get elevation from national map
+            s_elev = archive.get_nm_elev(s_db.lat.median(), s_db.lon.median()) 
+            s_xml.survey.elev_min = s_elev
+            s_xml.survey.elev_max = s_elev
+            
+            # start and end time
+            s_xml.survey.begin_date = s_db.start_date.min()
+            s_xml.survey.end_date = s_db.stop_date.max()
+            
+            # add list of files
+            s_xml.supplement_info += '\n\t\t\tFile List:\n\t\t\t'+'\n\t\t\t'.join(asc_fn_list)
+            
+            # write station xml
+            s_xml.write_xml_file(os.path.join(station_save_dir, 
+                                              '{0}_meta.xml'.format(station.upper())))
         
-        # make xml file
-        s_xml = archive.XMLMetadata()
-        s_xml.read_config_file(survey_cfg)
-        s_xml.supplement_info = s_xml.supplement_info.replace('\\n', '\n\t\t\t')
-        
-        # location
-        s_xml.survey.east = s_db.lon.median()
-        s_xml.survey.west = s_db.lon.median()
-        s_xml.survey.north = s_db.lat.median()
-        s_xml.survey.south = s_db.lat.median()
-        
-        s_elev = archive.get_nm_elev(s_db.lat.median(), s_db.lon.median()) 
-        s_xml.survey.elev_min = s_elev
-        s_xml.survey.elev_max = s_elev
-        
-        # start and end time
-        s_xml.survey.begin_date = s_db.start_date.min()
-        s_xml.survey.end_date = s_db.stop_date.max()
-        
-        # add list of files
-        s_xml.supplement_info += '\n\t\t\tFile List:\n\t\t\t'+'\n\t\t\t'.join(asc_fn_list)
-        
-        # write station xml
-        s_xml.write_xml_file(os.path.join(station_save_dir, 
-                                          '{0}_meta.xml'.format(station.upper())))
+        #--> write log file
+        log_fid = open(os.path.join(station_save_dir, 
+                                    '{0}_Archive.log'.format(station)), 'w')
+        log_fid.write('\n'.join(output))
+        log_fid.close()
 
 # adjust survey information to align with data        
 survey_cfg = archive.USGScfg()
@@ -96,14 +123,20 @@ survey_xml.survey.west = survey_db.lon.max()
 survey_xml.survey.south = survey_db.lat.min()
 survey_xml.survey.north = survey_db.lat.max()
 
+# get elevation min and max from station locations, not sure if this is correct
+survey_xml.survey.elev_min = survey_db.nm_elev.min()
+survey_xml.survey.elev_max = survey_db.nm_elev.max()
+
 # dates
 survey_xml.survey.begin_date = survey_db.start_date.min()
 survey_xml.survey.end_date = survey_db.stop_date.max()
 
+### --> write survey xml file
 survey_xml.write_xml_file(os.path.join(save_dir, 
                                        '{0}.xml'.format(survey)))
 
+# print timing
 et = datetime.datetime.now()
 t_diff = et-st
-print('--> Archiving tool: {0} seconds'.format(t_diff.total_seconds()))
+print('--> Archiving took: {0} seconds'.format(t_diff.total_seconds()))
         
