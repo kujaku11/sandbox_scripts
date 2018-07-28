7494e4ac35945c19027561c835a012cc3afb450a
JP <peacock.jared@gmail.com>
2018-07-27 18:36:41 -0700

    adding scripts

---
+++ archive_in_science_base_imush.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 12 12:55:59 2018
+
+@author: jpeacock
+"""
+import os
+import sys
+from cStringIO import StringIO
+import mtpy.usgs.usgs_archive as archive
+import datetime
+
+# =============================================================================
+# Inputs
+# =============================================================================
+#survey_dir = r"/mnt/hgfs/MTData/iMUSH_Zen_samples/imush"
+#survey_cfg = r"/mnt/hgfs/MTData/iMUSH_Zen_samples/imush_archive_PAB.cfg"
+
+survey_dir = r"d:\iMUSH"
+survey_cfg = r"d:\iMUSH\imush_archive_PAB.cfg"
+#survey_csv = r"c:\Users\jpeacock\Documents\imush\Archive\survey_summary_edit.csv"
+
+survey = 'iMUSH'
+stem = 'msh'
+#declination = 15.5
+declination = 15.5
+
+save_dir = os.path.join(survey_dir, 'Archive')
+if not os.path.exists(save_dir):
+    os.mkdir(save_dir)
+    
+# =============================================================================
+# class for capturing the output to store in a file
+# =============================================================================
+# this should capture all the print statements
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        sys.stdout = self._stdout
+# =============================================================================
+# make the survey xml file
+# =============================================================================
+survey_xml = archive.XMLMetadata()
+survey_xml.read_config_file(survey_cfg)
+
+st = datetime.datetime.now()
+for station in os.listdir(survey_dir):
+    station_path = os.path.join(survey_dir, station)
+    station_save_dir = os.path.join(save_dir, stem+station)
+    
+    if os.path.isdir(station_path) and len(station) == 4:
+        zc = archive.Z3DCollection()
+        # check to see if there are .z3d files in the folder, if not continue
+        try:
+            fn_list = zc.get_time_blocks(station_path)
+        except IndexError:
+            print('*** Skipping folder {0} ***'.format(station))
+            continue
+        
+        # make station folder
+        if not os.path.exists(station_save_dir):
+            os.mkdir(station_save_dir)
+        
+        zm = archive.USGSasc()
+        zm.CoordinateSystem = 'Geographic North'
+        zm.declination = declination
+        asc_fn_list = ['{0}{1}'.format(stem+station, ext) for ext in 
+                       ['.edi', '.png']]
+        
+        s_st = datetime.datetime.now()
+        # capture the output to put into a log file for each station, just to
+        # be sure and capture what happened.
+        with Capturing() as output:
+            for fn_block in fn_list:
+                zm.get_z3d_db(fn_block)
+                mtft_find = zm.get_metadata_from_mtft24_cfg()
+                
+                zm.SurveyID = survey
+                zm.SiteID = stem+zm.SiteID
+                # get metadata from survey summary file
+                #mtft_find = zm.get_metadata_from_survey_csv(survey_csv)
+                
+                for key in zm.channel_dict.keys():
+                    zm.channel_dict[key]['InstrumentID'] = 'ZEN'+zm.channel_dict[key]['InstrumentID']
+#                zm.write_asc_file(save_dir=station_save_dir,
+#                                  full=False, compress=True)
+#                asc_fn_list.append(os.path.basename(zm._make_file_name(save_path=station_save_dir, 
+#                                                      compression=True)))
+                zm.write_station_info_metadata(save_dir=station_save_dir,
+                                               mtft_bool=mtft_find)
+                
+            # make a station database
+            s_cfg = archive.USGScfg()
+            s_db, csv_fn = s_cfg.combine_run_cfg(station_save_dir)
+            
+            # make xml file
+            s_xml = archive.XMLMetadata()
+            s_xml.read_config_file(survey_cfg)
+            s_xml.supplement_info = s_xml.supplement_info.replace('\\n', '\n\t\t\t')
+            
+            # add station name to title
+            s_xml.title += ', station {0}'.format(stem+station)
+            
+            # location
+            s_xml.survey.east = s_db.lon.median()
+            s_xml.survey.west = s_db.lon.median()
+            s_xml.survey.north = s_db.lat.median()
+            s_xml.survey.south = s_db.lat.median()
+            
+            # get elevation from national map
+            s_elev = archive.get_nm_elev(s_db.lat.median(), s_db.lon.median()) 
+            s_xml.survey.elev_min = s_elev
+            s_xml.survey.elev_max = s_elev
+            
+            # start and end time
+            s_xml.survey.begin_date = s_db.start_date.min()
+            s_xml.survey.end_date = s_db.stop_date.max()
+            
+            # add list of files
+            s_xml.supplement_info += '\n\t\t\tFile List:\n\t\t\t'+'\n\t\t\t'.join(asc_fn_list)
+            
+            # write station xml
+            s_xml.write_xml_file(os.path.join(station_save_dir, 
+                                              '{0}_meta.xml'.format(stem+station)))
+        
+        #--> write log file
+        log_fid = open(os.path.join(station_save_dir, 
+                                    '{0}_Archive.log'.format(stem+station)), 'w')
+        log_fid.write('\n'.join(output))
+        log_fid.close()
+        
+        s_et = datetime.datetime.now()
+        station_diff = s_et - s_st
+        
+        print('--> Archived station {0}, took {1} seconds'.format(station, 
+                                              station_diff.total_seconds()))
+
+# adjust survey information to align with data        
+survey_cfg = archive.USGScfg()
+survey_db, csv_fn, location_fn = survey_cfg.combine_all_station_info(save_dir)
+survey_xml.supplement_info = survey_xml.supplement_info.replace('\\n', '\n\t\t\t')
+survey_cfg.write_shp_file(csv_fn, save_path=save_dir)
+
+# location
+survey_xml.survey.east = survey_db.lon.min()
+survey_xml.survey.west = survey_db.lon.max()
+survey_xml.survey.south = survey_db.lat.min()
+survey_xml.survey.north = survey_db.lat.max()
+
+# get elevation min and max from station locations, not sure if this is correct
+survey_xml.survey.elev_min = survey_db.nm_elev.min()
+survey_xml.survey.elev_max = survey_db.nm_elev.max()
+
+# dates
+survey_xml.survey.begin_date = survey_db.start_date.min()
+survey_xml.survey.end_date = survey_db.stop_date.max()
+
+### --> write survey xml file
+survey_xml.write_xml_file(os.path.join(save_dir, 
+                                       '{0}.xml'.format(stem+survey)))
+
+# print timing
+et = datetime.datetime.now()
+t_diff = et-st
+print('--> Archiving took: {0} seconds'.format(t_diff.total_seconds()))
+        
---
+++ move_and_rename_z3d_files_imush.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 27 17:39:21 2018
+
+@author: jpeacock
+"""
+
+import os
+import shutil
+import mtpy.usgs.zen as zen
+
+main_dir = r"d:\iMUSH"
+
+notes = ['# Notes for stations']
+         
+for survey in os.listdir(main_dir):
+    survey_dir = os.path.join(main_dir, survey)
+    if os.path.isdir(survey_dir):
+        for station in os.listdir(survey_dir):
+            station_dir = os.path.join(survey_dir, station)
+            if os.path.isdir(station_dir) and len(station) >= 4 and \
+               station not in ['do_not_archive']:
+                if station.find('_') > 0 and len(station) >= 4:
+                    notes.append(station)
+                station = station[0:4].upper()
+                
+                # make a folder in the main directory to save to
+                sv_path = os.path.join(main_dir, station)
+                if not os.path.exists(sv_path):
+                    os.mkdir(sv_path)
+                print('--- {0}'.format(sv_path))
+                # look for all Z3D files within a station folder
+                for root, folders, files in os.walk(station_dir):
+                    for fn in files:
+                        if fn.endswith('.Z3D'):
+                            fn_path = os.path.join(root, fn)
+                            # skip the small files
+                            if os.stat(fn_path).st_size < 350000L:
+                                continue
+                            if fn.count('_') == 4:
+                                sv_fn = '_'.join([station]+fn.split('_')[1:])
+                                shutil.move(fn_path, 
+                                            os.path.join(sv_path, sv_fn))
+                            else:
+                                # read in just the metadata
+                                z_obj = zen.Zen3D(fn_path)
+                                z_obj.read_all_info()
+                                z_obj.station = station
+                                try:
+                                    channel = z_obj.metadata.ch_cmp.upper()
+                                except AttributeError:
+                                    notes.append('***CHECK {0}'.format(fn_path))
+                                    continue
+                                st = z_obj.schedule.Time.replace(':','')
+                                sd = z_obj.schedule.Date.replace('-','')
+                                # make a new useful file name
+                                sv_fn = '{0}_{1}_{2}_{3}_{4}.Z3D'.format(station, 
+                                                                         sd, 
+                                                                         st,
+                                                                         int(z_obj.df),
+                                                                         channel)
+                                # rename that file instead of copy, way faster
+                                shutil.move(fn_path, 
+                                            os.path.join(sv_path, sv_fn))
+    #                            os.rename(fn_path, 
+    #                                      os.path.join(station_path, sv_fn))
+                        elif 'mtft24.cfg' in fn:
+                            shutil.move(os.path.join(root, fn),
+                                        os.path.join(sv_path, 
+                                                     '{0}_mtft24.cfg'.format(station)))
+                        else:
+                            continue
+    
+### write out notes
+with open(os.path.join(main_dir, 'station_notes.txt'), 'w') as fid:
+    fid.write('\n'.join(notes))
+                    
+                
