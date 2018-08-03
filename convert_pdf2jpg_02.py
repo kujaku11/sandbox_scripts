312b90bd055b1fa8f947161b52e8772b6c5ed25b
JP <peacock.jared@gmail.com>
2018-08-02 19:19:57 -0700

    adding scripts

--- convert_pdf2jpg_02.py
+++ convert_pdf2jpg_02.py
@@ -8,10 +8,10 @@ Created on Tue Jun 19 10:17:28 2018
 import subprocess
 import os
 
-dir_path = r"c:\Users\jpeacock\Documents\ClearLake\Figures"
+dir_path = r"c:\Users\jpeacock\Documents\TexDocs\Posters\EMIW_2018_mountain_pass"
 
 for fn in [os.path.join(dir_path, ff) for ff in os.listdir(dir_path) if ff.endswith('.pdf')]:
-    if os.path.getctime(fn) > 1533170000.:
+    if os.path.getctime(fn) > 1533252000.:
         std_out = subprocess.check_call(['magick',
                                          '-density','300',
                                          fn,
--- make_kml_from_brod_file.py
+++ make_kml_from_brod_file.py
@@ -8,7 +8,7 @@ Created on Wed Dec 03 10:40:47 2014
 import simplekml as skml
 import os
 import pyproj
-csv_fn = r"c:\Users\jpeacock\Downloads\Bodie-Aurora-MT-stations.txt"
+csv_fn = r"c:\Users\jpeacock\Downloads\br_Mono_Basin_WGS84.txt"
 
 with open(csv_fn, 'r') as fid:
     csv_lines = fid.readlines()
@@ -17,8 +17,8 @@ with open(csv_fn, 'r') as fid:
 
 kml_obj = skml.Kml()
 
-p_nad27 = pyproj.Proj(init='epsg:4267')
-p_wgs84 = pyproj.Proj(init='epsg:4326')
+#p_nad27 = pyproj.Proj(init='epsg:4267')
+#p_wgs84 = pyproj.Proj(init='epsg:4326')
 
 for ii, line in enumerate(csv_lines[2:]):
     csv_list = line.strip().split()
@@ -27,10 +27,12 @@ for ii, line in enumerate(csv_lines[2:]):
     csv_list = [cc.strip() for cc in csv_list]
 #    station = 'HF{0:02}'.format(int(csv_list[0].split()[-1]))
     station = 'BR{0}'.format(csv_list[0])
-    lon_27 = -1*float(csv_list[2][1:])
-    lat_27 = float(csv_list[1][1:])
+    lat_84 = float(csv_list[1])
+    lon_84 = float(csv_list[2])
+    #lon_27 = -1*float(csv_list[2][1:])
+    #lat_27 = float(csv_list[1][1:])
     
-    lon_84, lat_84 = pyproj.transform(p_nad27, p_wgs84, lon_27, lat_27)
+    #lon_84, lat_84 = pyproj.transform(p_nad27, p_wgs84, lon_27, lat_27)
     pnt = kml_obj.newpoint(name=station, 
                            coords=[(lon_84, lat_84, 0.0)])
     pnt.style.labelstyle.color = skml.Color.white
@@ -40,7 +42,7 @@ for ii, line in enumerate(csv_lines[2:]):
 
 #kml_obj.save(csv_fn[:-4]+'.kml')
 kml_obj.save(os.path.join(os.path.dirname(csv_fn), 
-                          'br_mt_sites_2016.kml'))
+                          'br_mt_sites_2017.kml'))
     
             
             
