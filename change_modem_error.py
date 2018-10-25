07c3d5250b10557529d029a56bb18fa161eea13e
JP <peacock.jared@gmail.com>
2018-10-24 18:46:02 -0700

    adding scripts

--- change_modem_error.py
+++ change_modem_error.py
@@ -9,7 +9,7 @@ import mtpy.modeling.modem as modem
 import os
 
 #dfn = r"c:\Users\jpeacock\Documents\iMush\modem_inv\shz_inv_01\shz_modem_data_err03_tip02.dat"
-dfn = r"c:\Users\jpeacock\Documents\MountainPass\modem_inv\inv_07\mp_modem_data_z03_topo_edit.dat"
+dfn = r"c:\Users\jpeacock\Documents\MountainPass\MusicValley\modem_inv\inv_03\mv_modem_data_ef06.dat"
 sv_fn = os.path.basename(dfn)[0:os.path.basename(dfn).find('_')]
 
 d_obj = modem.Data()
@@ -18,7 +18,7 @@ d_obj.read_data_file(dfn)
 d_obj.error_type_z = 'eigen_floor'
 d_obj.error_type_tipper = 'abs_floor'
 d_obj.inv_mode = '2'
-d_obj.error_value_z = 5.0
+d_obj.error_value_z = 6.0
 d_obj.error_value_tipper = .04
 
 
@@ -29,7 +29,7 @@ if d_obj.inv_mode == '2':
                                           d_obj.error_value_z),
                           fill=False,
                           compute_error=True,
-                          elevation=True)
+                          elevation=False)
 elif d_obj.inv_mode == '5':
     d_obj.write_data_file(save_path=os.path.dirname(dfn),
                           fn_basename='{0}_modem_data_tip{1:02.0f}.dat'.format(
