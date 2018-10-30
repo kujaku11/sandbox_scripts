732f485ba52952f5142bb5de7fabfce83169281b
JP <peacock.jared@gmail.com>
2018-10-30 14:29:32 -0700

    adding scripts

--- adjust_sciencebase_page_imush.py
+++ adjust_sciencebase_page_imush.py
@@ -21,6 +21,11 @@ page_url = '5ad77f06e4b0e2c2dd25e798'
 username = 'jpeacock@usgs.gov'
 password = getpass.getpass()
 
+ng_citation = 'Bedrosian, P. A., Peacock, J. R., Bowles-Martinez, E., '+\
+              'Schultz, A., Hill, G. J. (2018) Crustal inheritance '+\
+              'and a top-down control on arc magmatism, Nature Geoscience, '+\
+              '11, p. 865-870.' 
+
 # =============================================================================
 # login and get child ids
 # =============================================================================
@@ -29,15 +34,53 @@ sb_session.login(username, password)
 
 child_ids = sb_session.get_child_ids(page_url)
 # =============================================================================
-# Change citation
+# Change json
 # =============================================================================
 for child_id in child_ids:
-    child_json = sb_session.get_item(child_id)
+    try:
+        child_json = sb_session.get_item(child_id)
+    except:
+        print('---> skipping child id {0}'.format(child_id))
+        continue
     
+    ### remove the double doi in the citation
     https_find_01 = child_json['citation'].find('https')
     https_find_02 = child_json['citation'].find('https', https_find_01+5)
     
     child_json['citation'] = child_json['citation'][0:https_find_01]+\
                              child_json['citation'][https_find_02:]
     
-    sb_session.update_item(child_json)
+    ### sort order of files
+    fn_dict = {'zip':[]}
+    for f_dict in child_json['files']:
+        fname = f_dict['name']
+        if fname.endswith('.edi'):
+            fn_dict['edi'] = f_dict
+        elif fname.endswith('.png'):
+            fn_dict['png'] = f_dict
+        elif fname.endswith('xml'):
+            fn_dict['xml'] = f_dict
+        elif fname.endswith('zip'):
+            fn_dict['zip'].append(f_dict)
+        else:
+            continue 
+        
+    # sort zip files by date
+    zip_fn_list = sorted(fn_dict['zip'], key=lambda k: k['name'])
+    
+    # make new list of file dictionaries
+    fn_list = [fn_dict['xml'], fn_dict['edi'], fn_dict['png']]
+    child_json['files'] = fn_list + zip_fn_list
+    
+    ### change web links
+    child_json['webLinks'][0]['type'] = 'Publication that references this resource'
+    child_json['webLinks'][0]['title'] = 'Crustal inheritance and a top-down '+\
+                                         'control on arc magmatism at Mount'+\
+                                         'St. Helens, in Nature Geoscience'
+    
+    ### update the child item, sometimes you need to try twice
+    try:
+        updated_child = sb_session.update_item(child_json)
+    except:
+        updated_child = sb_session.update_item(child_json)
+    
