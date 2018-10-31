5234755ae646c8020fd240ce4ef8b7ae9ccf3a3d
JP <peacock.jared@gmail.com>
2018-10-30 19:45:05 -0700

    adding scripts

--- adjust_sciencebase_page_imush.py
+++ adjust_sciencebase_page_imush.py
@@ -23,8 +23,8 @@ password = getpass.getpass()
 
 ng_citation = 'Bedrosian, P. A., Peacock, J. R., Bowles-Martinez, E., '+\
               'Schultz, A., Hill, G. J. (2018) Crustal inheritance '+\
-              'and a top-down control on arc magmatism, Nature Geoscience, '+\
-              '11, p. 865-870.' 
+              'and a top-down control on arc magmatism at Mount St. Helens,'+\
+              ' Nature Geoscience, 11, p. 865-870.' 
 
 # =============================================================================
 # login and get child ids
@@ -43,40 +43,38 @@ for child_id in child_ids:
         print('---> skipping child id {0}'.format(child_id))
         continue
     
-    ### remove the double doi in the citation
-    https_find_01 = child_json['citation'].find('https')
-    https_find_02 = child_json['citation'].find('https', https_find_01+5)
-    
-    child_json['citation'] = child_json['citation'][0:https_find_01]+\
-                             child_json['citation'][https_find_02:]
-    
-    ### sort order of files
-    fn_dict = {'zip':[]}
-    for f_dict in child_json['files']:
-        fname = f_dict['name']
-        if fname.endswith('.edi'):
-            fn_dict['edi'] = f_dict
-        elif fname.endswith('.png'):
-            fn_dict['png'] = f_dict
-        elif fname.endswith('xml'):
-            fn_dict['xml'] = f_dict
-        elif fname.endswith('zip'):
-            fn_dict['zip'].append(f_dict)
-        else:
-            continue 
-        
-    # sort zip files by date
-    zip_fn_list = sorted(fn_dict['zip'], key=lambda k: k['name'])
-    
-    # make new list of file dictionaries
-    fn_list = [fn_dict['xml'], fn_dict['edi'], fn_dict['png']]
-    child_json['files'] = fn_list + zip_fn_list
+#    ### remove the double doi in the citation
+#    https_find_01 = child_json['citation'].find('https')
+#    https_find_02 = child_json['citation'].find('https', https_find_01+5)
+#    
+#    child_json['citation'] = child_json['citation'][0:https_find_01]+\
+#                             child_json['citation'][https_find_02:]
+#    
+#    ### sort order of files
+#    fn_dict = {'zip':[]}
+#    for f_dict in child_json['files']:
+#        fname = f_dict['name']
+#        if fname.endswith('.edi'):
+#            fn_dict['edi'] = f_dict
+#        elif fname.endswith('.png'):
+#            fn_dict['png'] = f_dict
+#        elif fname.endswith('xml'):
+#            fn_dict['xml'] = f_dict
+#        elif fname.endswith('zip'):
+#            fn_dict['zip'].append(f_dict)
+#        else:
+#            continue 
+#        
+#    # sort zip files by date
+#    zip_fn_list = sorted(fn_dict['zip'], key=lambda k: k['name'])
+#    
+#    # make new list of file dictionaries
+#    fn_list = [fn_dict['xml'], fn_dict['edi'], fn_dict['png']]
+#    child_json['files'] = fn_list + zip_fn_list
     
     ### change web links
     child_json['webLinks'][0]['type'] = 'Publication that references this resource'
-    child_json['webLinks'][0]['title'] = 'Crustal inheritance and a top-down '+\
-                                         'control on arc magmatism at Mount'+\
-                                         'St. Helens, in Nature Geoscience'
+    child_json['webLinks'][0]['title'] = ng_citation
     
     ### update the child item, sometimes you need to try twice
     try:
---
+++ sb_check_stats.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Check database statistics for a science base archive
+
+Created on Tue Oct 30 18:58:29 2018
+
+@author: jpeacock
+"""
+# =============================================================================
+# Imports
+# =============================================================================
+import sciencebasepy as sb
+import getpass
+import pandas as pd
+
+# =============================================================================
+# Parameters
+# =============================================================================
+page_url = '5ad77f06e4b0e2c2dd25e798'
+username = 'jpeacock@usgs.gov'
+password = getpass.getpass()
+
+# =============================================================================
+# login and get child ids
+# =============================================================================
+sb_session = sb.SbSession()
+sb_session.login(username, password)
+
+child_ids = sb_session.get_child_ids(page_url)
+# =============================================================================
+# Change json
+# =============================================================================
+data = []
+
+for child_id in child_ids:
+    try:
+        child_json = sb_session.get_item(child_id)
+    except:
+        print('---> skipping child id {0}'.format(child_id))
+        continue
+    
+    # get station name from title
+    station = child_json['title'].split()[1]
+    
+    # get the total size of all the files
+    total_file_size = sum([fn_dict['size'] for fn_dict in child_json['files']])
+    
+    # get the number of files
+    n_files = len(child_json['files'])
+    
+    # check to make child has all the files
+    fn_list = [fn_dict['name'] for fn_dict in child_json['files']]
+    has_edi = False
+    has_png = False
+    has_xml = False
+    has_zips = False
+    for fn in fn_list:
+        if fn.endswith('xml'):
+            has_xml = True
+        elif fn.endswith('edi'):
+            has_edi = True
+        elif fn.endswith('png'):
+            has_png = True
+        elif fn.endswith('zip'):
+            has_zips = True
+        else:
+            pass
+        
+    data.append((station, n_files, total_file_size, has_xml, has_edi,
+                 has_png, has_zips))
+
+# =============================================================================
+# make data frame and write to csv
+# =============================================================================
+df = pd.DataFrame(data, columns=('station', 'n_files', 'size', 'xml', 'edi',
+                                 'png', 'zips'))
+
+df.to_csv(r"c:\Users\jpeacock\Documents\iMush\sb_stats.csv", index=False)
+        
+    
+
+    
