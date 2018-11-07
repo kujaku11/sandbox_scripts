198ef3c4baf0ea0f0c5e9af0999e35f4451eb2cf
JP <peacock.jared@gmail.com>
2018-11-07 15:14:01 -0800

    adding scripts

--- change_modem_error.py
+++ change_modem_error.py
@@ -7,9 +7,10 @@ Created on Thu Feb 23 18:47:08 2017
 
 import mtpy.modeling.modem as modem
 import os
+import numpy as np
 
 #dfn = r"c:\Users\jpeacock\Documents\iMush\modem_inv\shz_inv_01\shz_modem_data_err03_tip02.dat"
-dfn = r"c:\Users\jpeacock\Documents\SaudiArabia\modem_inv\inv_kh\kh_modem_data_z03_t02_edit.dat"
+dfn = r"c:\Users\jpeacock\Documents\SaudiArabia\modem_inv\inv_all\rh_modem_data_z03_t02_edit.dat"
 sv_fn = os.path.basename(dfn)[0:os.path.basename(dfn).find('_')]
 
 d_obj = modem.Data()
@@ -21,6 +22,11 @@ d_obj.inv_mode = '2'
 d_obj.error_value_z = 6.0
 d_obj.error_value_tipper = .04
 
+### add error to certain stations
+err_stations = ['med905', 'med906', 'med907', 'med910', 'med914']
+for e_station in err_stations:
+    s_find = np.where(d_obj.data_array['station'] == e_station)[0][0]
+    d_obj.data_array[s_find]['z_err'] *= 5.0 
 
 if d_obj.inv_mode == '2':
     d_obj.write_data_file(save_path=os.path.dirname(dfn),
