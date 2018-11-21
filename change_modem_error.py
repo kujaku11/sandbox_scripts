21b73f86075ebd4f9e8cb27b1d15b89d173c14fa
JP <peacock.jared@gmail.com>
2018-11-20 18:43:10 -0800

    adding scripts

--- change_modem_error.py
+++ change_modem_error.py
@@ -10,8 +10,9 @@ import os
 import numpy as np
 
 #dfn = r"c:\Users\jpeacock\Documents\iMush\modem_inv\shz_inv_01\shz_modem_data_err03_tip02.dat"
-dfn = r"c:\Users\jpeacock\Documents\SaudiArabia\modem_inv\inv_all\rh_modem_data_z03_t02_edit.dat"
+dfn = r"c:\Users\jpeacock\Documents\MountainPass\modem_inv\inv_07\mp_modem_data_z03_topo_edit.dat"
 sv_fn = os.path.basename(dfn)[0:os.path.basename(dfn).find('_')]
+elevation_bool = True
 
 d_obj = modem.Data()
 d_obj.read_data_file(dfn)
@@ -19,14 +20,16 @@ d_obj.read_data_file(dfn)
 d_obj.error_type_z = 'eigen_floor'
 d_obj.error_type_tipper = 'abs_floor'
 d_obj.inv_mode = '2'
-d_obj.error_value_z = 6.0
+d_obj.error_value_z = 5.0
 d_obj.error_value_tipper = .04
 
+
+
 ### add error to certain stations
-err_stations = ['med905', 'med906', 'med907', 'med910', 'med914']
+err_stations = ['mp320', 'mp336']
 for e_station in err_stations:
     s_find = np.where(d_obj.data_array['station'] == e_station)[0][0]
-    d_obj.data_array[s_find]['z_err'] *= 5.0 
+    d_obj.data_array[s_find]['z_err'] *= 7.0 
 
 if d_obj.inv_mode == '2':
     d_obj.write_data_file(save_path=os.path.dirname(dfn),
@@ -35,7 +38,7 @@ if d_obj.inv_mode == '2':
                                           d_obj.error_value_z),
                           fill=False,
                           compute_error=True,
-                          elevation=False)
+                          elevation=elevation_bool)
 elif d_obj.inv_mode == '5':
     d_obj.write_data_file(save_path=os.path.dirname(dfn),
                           fn_basename='{0}_modem_data_tip{1:02.0f}.dat'.format(
@@ -43,7 +46,7 @@ elif d_obj.inv_mode == '5':
                                           d_obj.error_value_tipper*100),
                           fill=False,
                           compute_error=True,
-                          elevation=True)
+                          elevation=elevation_bool)
 else:
     d_obj.write_data_file(save_path=os.path.dirname(dfn),
                           fn_basename='{0}_modem_data_ef{1:02.0f}_tip{2:02.0f}.dat'.format(
@@ -52,4 +55,4 @@ else:
                                           d_obj.error_value_tipper*100),
                           fill=False,
                           compute_error=True,
-                          elevation=True)
+                          elevation=elevation_bool)
