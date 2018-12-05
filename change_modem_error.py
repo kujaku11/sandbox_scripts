99e48ab450c99305b59749efafe8c139de4ccd1b
JP <peacock.jared@gmail.com>
2018-12-04 17:40:30 -0800

    adding scripts

--- change_modem_error.py
+++ change_modem_error.py
@@ -10,7 +10,7 @@ import os
 import numpy as np
 
 #dfn = r"c:\Users\jpeacock\Documents\iMush\modem_inv\shz_inv_01\shz_modem_data_err03_tip02.dat"
-dfn = r"c:\Users\jpeacock\Documents\SaudiArabia\modem_inv\inv_n30w_topo\med_modem_data_z03_t02_topo_edit.dat"
+dfn = r"c:\Users\jpeacock\Documents\Geothermal\Camas\modem_inv\inv_02\cm_modem_data_z03_t02_edit.dat"
 sv_fn = os.path.basename(dfn)[0:os.path.basename(dfn).find('_')]
 elevation_bool = True
 
@@ -21,15 +21,15 @@ d_obj.error_type_z = 'eigen_floor'
 d_obj.error_type_tipper = 'abs_floor'
 d_obj.inv_mode = '5'
 d_obj.error_value_z = 6.0
-d_obj.error_value_tipper = .04
+d_obj.error_value_tipper = .03
 
 
 
 ### add error to certain stations
-err_stations = ['med133', 'med312']
+err_stations = ['CM211']
 for e_station in err_stations:
     s_find = np.where(d_obj.data_array['station'] == e_station)[0][0]
-    d_obj.data_array[s_find]['z_err'] *= 7.0 
+    d_obj.data_array[s_find]['z_err'] *= 5.0 
 
 if d_obj.inv_mode == '2':
     d_obj.write_data_file(save_path=os.path.dirname(dfn),
