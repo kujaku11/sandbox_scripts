9df63b4709ba6cfe375fc2ca990514a260e6e0be
JP <peacock.jared@gmail.com>
2018-12-12 10:50:40 -0800

    adding scripts

--- change_modem_error.py
+++ change_modem_error.py
@@ -15,15 +15,15 @@ import numpy as np
 # Inputs
 # =============================================================================
 #dfn = r"c:\Users\jpeacock\Documents\iMush\modem_inv\shz_inv_01\shz_modem_data_err03_tip02.dat"
-dfn = r"c:\Users\jpeacock\Documents\SaudiArabia\modem_inv\inv_n30w_topo\med_modem_data_z03_t02_topo_edit.dat"
+dfn = r"c:\Users\jpeacock\Documents\SaudiArabia\modem_inv\med_modem_data_z03_t02_edit.dat"
 
-remove_stations = ['med510']
+remove_stations = None
 shady_stations = ['med133', 'med312']
-add_err = 10
+add_err = 5
 elevation_bool = True
 
-inv_modes = ['1', '2', '5']
-z_err_value = 8.0
+inv_modes = ['2']
+z_err_value = 5.0
 t_err_value = .03
 z_err_type = 'eigen_floor'
 t_err_type = 'abs_floor'
@@ -41,12 +41,13 @@ for e_station in shady_stations:
     d_obj.data_array[s_find]['z_err'] *= add_err 
     
 ### remove a bad station
-for b_station in remove_stations:
-    s_find = np.where(d_obj.data_array['station'] == b_station)[0][0]
-    d_obj.data_array[s_find]['z'][:] = 0 
-    d_obj.data_array[s_find]['z_err'][:] = 0 
-    d_obj.data_array[s_find]['tip'][:] = 0 
-    d_obj.data_array[s_find]['tip_err'][:] = 0 
+if remove_stations is not None:
+    for b_station in remove_stations:
+        s_find = np.where(d_obj.data_array['station'] == b_station)[0][0]
+        d_obj.data_array[s_find]['z'][:] = 0 
+        d_obj.data_array[s_find]['z_err'][:] = 0 
+        d_obj.data_array[s_find]['tip'][:] = 0 
+        d_obj.data_array[s_find]['tip_err'][:] = 0 
     
 
 for inv_mode in inv_modes:
@@ -60,7 +61,7 @@ for inv_mode in inv_modes:
     
     if d_obj.inv_mode == '2':
         d_obj.write_data_file(save_path=os.path.dirname(dfn),
-                              fn_basename='{0}_modem_data_ef{1:02.0f}.dat'.format(
+                              fn_basename='{0}_modem_data_z{1:02.0f}.dat'.format(
                                               sv_fn, 
                                               d_obj.error_value_z),
                               fill=False,
@@ -68,7 +69,7 @@ for inv_mode in inv_modes:
                               elevation=elevation_bool)
     elif d_obj.inv_mode == '5':
         d_obj.write_data_file(save_path=os.path.dirname(dfn),
-                              fn_basename='{0}_modem_data_tip{1:02.0f}.dat'.format(
+                              fn_basename='{0}_modem_data_t{1:02.0f}.dat'.format(
                                               sv_fn, 
                                               d_obj.error_value_tipper*100),
                               fill=False,
@@ -76,7 +77,7 @@ for inv_mode in inv_modes:
                               elevation=elevation_bool)
     else:
         d_obj.write_data_file(save_path=os.path.dirname(dfn),
-                              fn_basename='{0}_modem_data_ef{1:02.0f}_tip{2:02.0f}.dat'.format(
+                              fn_basename='{0}_modem_data_z{1:02.0f}_t{2:02.0f}.dat'.format(
                                               sv_fn, 
                                               d_obj.error_value_z,
                                               d_obj.error_value_tipper*100),
