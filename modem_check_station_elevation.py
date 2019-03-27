87f40909a24935bf9cbca16477018f0abbc740fa
JP <peacock.jared@gmail.com>
2019-03-27 09:06:06 -0700

    adding scripts

---
+++ modem_check_station_elevation.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 08:56:50 2019
+
+@author: jpeacock
+"""
+
+from mtpy.modeling import modem
+
+mfn = r"c:\Users\jpeacock\Documents\Geysers\gz_sm50_topo_ocean.rho"
+dfn = r"c:\Users\jpeacock\Documents\Geysers\gz_modem_data_r50_z05_topo_edit.dat"
+
+d_obj = modem.Data()
+d_obj.read_data_file(dfn)
+
+m_obj = modem.Model()
+m_obj.read_model_file(mfn)
+
+d_obj.project_stations_on_topography(m_obj)
