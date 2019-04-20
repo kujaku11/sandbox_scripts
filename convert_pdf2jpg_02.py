8975f912c7a339337af82d4a922daef3ac2284cc
JP <peacock.jared@gmail.com>
2019-04-19 17:27:08 -0700

    adding scripts

--- convert_pdf2jpg_02.py
+++ convert_pdf2jpg_02.py
@@ -26,7 +26,7 @@ def convert_pdt_to_jpg(fn):
                                             os.path.basename(fn[:-4]+'.jpg')))
     return std_out
 
-num_files = 20
+num_files = 1
 
 for ii, fn in enumerate(fn_list):
     if num_files is not None and ii < num_files:
@@ -34,6 +34,6 @@ for ii, fn in enumerate(fn_list):
     elif num_files is not None and ii >= num_files:
         break
     elif num_files is None:        
-        if os.path.getctime(fn) > round(time.time() - 15*60, -3):
+        if os.path.getctime(fn) > round(time.time() - 5*60, -3):
             std_out = convert_pdt_to_jpg(fn)
         
--- untitled0.py
+++ untitled0.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 10 12:01:06 2019
+Created on Fri Apr 19 14:39:53 2019
 
 @author: jpeacock
 """
 
-import numpy as np
-from matplotlib import pyplot as plt
-from mtpy.modeling import modem
+import os
+import pandas as pd
+import glob
 
-mfn = r"c:\Users\jpeacock\Documents\Geysers\modem_inv\inv03\gz_err03_cov02_NLCG_057.rho"
-
-rho = 45
-
-m_obj = modem.Model()
-m_obj.read_model_file(mfn)
-
+edi_dirs = ['c:\Users\jpeacock', 'd:\Peacock\MTData']
+fn_db = pd.DataFrame()
+for edi_dir in edi_dirs:
+    for root, dirs, fn_list in os.walk(edi_dir):
+        for fn in fn_list:
+            if glob.fnmatch(fn, '.edi'):
+                fn_dict = {}
+                fn_dict['fn'] = fn
+                fn_dict['fn_path'] = os.path.join(root, fn)
+                fn_dict['fn_date'] = os.path.getmtime(fn_dict['fn_path'])
+                fn_db.append(pd.DataFrame(fn_dict))
