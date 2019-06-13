5a6f7d92b025613fde7a35ccdd5d2e07ed5906a8
JP <peacock.jared@gmail.com>
2019-06-12 18:17:55 -0700

    adding scripts

--- make_1d_starting_model_mp.py
+++ make_1d_starting_model_mp.py
@@ -14,11 +14,13 @@ from mtpy.modeling import modem
 from mtpy.modeling import occam1d
 from scipy.interpolate import griddata
 import pickle
+import glob
 
 dfn = r"c:\Users\jpeacock\Documents\MountainPass\modem_inv\inv_08\mp_modem_data_z05.dat"
 mfn = r"c:\Users\jpeacock\Documents\MountainPass\modem_inv\inv_08\mp_sm02_topo.rho"
 occam1d_path = r"c:\MinGW32-xy\Peacock\occam\occam1d.exe"
 sv_dir = os.path.dirname(dfn)
+pickle_fn = os.path.join(sv_dir, 'mp_1d_res.pkl')
 # =============================================================================
 # read in data file and get mean and median of determinant
 # =============================================================================
@@ -31,54 +33,115 @@ m_obj.read_model_file(mfn)
 ocm = occam1d.Model()
 ocm.write_model_file(save_path=sv_dir, model_depth=m_obj.grid_z)
 
-res_arr = np.zeros((d_obj.data_array.shape[0],
-                    m_obj.res_model.shape[-1]))
+if os.path.isfile(pickle_fn) is False:
+    res_arr = np.zeros((d_obj.data_array.shape[0],
+                        m_obj.res_model.shape[-1]))
+    
+    x = np.zeros(d_obj.data_array.shape[0])
+    y = np.zeros(d_obj.data_array.shape[0])
+    for ii, d_arr in enumerate(d_obj.data_array):
+        x[ii] = d_arr['rel_north']
+        y[ii] = d_arr['rel_east']
+        d_index = np.where(m_obj.grid_z > d_arr['rel_elev'])[0][0]
+        s_dir = os.path.join(sv_dir, d_arr['station']) 
 
-x = np.zeros(d_obj.data_array.shape[0])
-y = np.zeros(d_obj.data_array.shape[0])
-for ii, d_arr in enumerate(d_obj.data_array):
-    x[ii] = d_arr['rel_north']
-    y[ii] = d_arr['rel_east']
-    z_obj = mtz.Z(d_arr['z'], freq=1./d_obj.period_list)
-    
-    d_index = np.where(m_obj.grid_z > d_arr['rel_elev'])[0][0]
-    
-    # make occam data file
-    ocd = occam1d.Data()
-    rp_tuple = (1./d_obj.period_list,
-                z_obj.res_det, z_obj.res_det_err,
-                z_obj.phase_det, z_obj.phase_det_err)
-    ocd.save_path = sv_dir
-    ocd.write_data_file(rp_tuple=rp_tuple,
-                       mode='det',
-                       res_err=5,
-                       phase_err=2.5)
-    ### make startup file
-    ocs = occam1d.Startup()
-    ocs.start_rho = z_obj.res_det.mean()
-    ocs.data_fn = ocd.data_fn
-    ocs.model_fn = ocm.model_fn
-    ocs.save_path = sv_dir
-    ocs.max_iter = 10
-    ocs.write_startup_file()
-    
-    occam1d.Run(ocs.startup_fn, occam_path=occam1d_path, mode='Det')
-    try:
-        m_ocm = occam1d.Model()
-        m_ocm.read_iter_file(os.path.join(sv_dir, 'Det_5.iter'), 
-                             ocm.model_fn)
-        res_arr[ii, d_index:] = m_ocm.model_res[1:-(d_index+2), 1]
-    except:
-        print('Occam did not run for {0}'.format(d_arr['station']))
+        if not os.path.exists(s_dir):
+            os.mkdir(s_dir)
 
+        ocm = occam1d.Model()
+        ocm.write_model_file(save_path=s_dir, model_depth=m_obj.grid_z)
+        
+        # make occam data file
+        z_obj = mtz.Z(d_arr['z'], freq=1./d_obj.period_list)
+        ocd = occam1d.Data()
+        if os.path.isfile(os.path.join(s_dir, 'Occam1d_DataFile_DET.dat')) is False:
+            
+            
+            rp_tuple = (1./d_obj.period_list,
+                        z_obj.res_det, z_obj.res_det_err,
+                        z_obj.phase_det, z_obj.phase_det_err)
+            ocd.save_path = s_dir
+            ocd.write_data_file(rp_tuple=rp_tuple,
+                               mode='det',
+                               res_err=5,
+                               phase_err=2.5)
+        else:
+            ocd.read_data_file(os.path.join(s_dir, 'Occam1d_DataFile_DET.dat'))
+            remove_files = glob.glob('{0}\*.iter'.format(s_dir)) + \
+                           glob.glob('{0}\*.resp'.format(s_dir)) 
+            for rfn in remove_files:
+                os.remove(rfn)
+                
+        ### make startup file
+        ocs = occam1d.Startup()
+        ocs.start_rho = z_obj.res_det.mean()
+        ocs.data_fn = ocd.data_fn
+        ocs.model_fn = ocm.model_fn
+        ocs.save_path = s_dir
+        ocs.max_iter = 10
+        ocs.write_startup_file()
+        
+        occam1d.Run(ocs.startup_fn, occam_path=occam1d_path, mode='Det')
+        try:
+            m_ocm = occam1d.Model()
+            m_ocm.read_iter_file(os.path.join(s_dir, 'Det_5.iter'), 
+                                 ocm.model_fn)
+            res_arr[ii, d_index:] = m_ocm.model_res[1:-(d_index+2), 1]
+            if os.path.join(sv_dir, '{0}.png'.format(d_arr['station'])) is False:
+                pr = occam1d.Plot1DResponse(data_te_fn=os.path.join(s_dir, 
+                                                                r"Occam1d_DataFile_DET.dat"),
+                                        resp_te_fn=os.path.join(s_dir, r"Det_5.resp"),
+                                        iter_te_fn=os.path.join(s_dir, r"Det_5.iter"),
+                                        model_fn=os.path.join(s_dir, r"Model1D"),
+                                        depth_limits=(-1, 30))
+                pr.save_figure(os.path.join(sv_dir,
+                                            '{0}.png'.format(d_arr['station'])),
+                               fig_dpi=600)
+        except:
+            print('Occam did not run for {0}'.format(d_arr['station']))
+    
+    with open(pickle_fn, 'wb') as fid:
+        pickle.dump(x, fid)
+        pickle.dump(y, fid)
+        pickle.dump(res_arr, fid)
+    
 # =============================================================================
 # make occam 1d model from model file
 # =============================================================================
-points = np.array([x, y])
+with open(pickle_fn, 'rb') as fid:
+    y = pickle.load(fid)
+    x = pickle.load(fid)
+    res_arr = pickle.load(fid)
 
-grid_x, grid_y = np.meshgrid(m_obj.grid_north, m_obj.grid_east)
+### clean up the array a bit
+# mp205 not quite right setting to nearby station
+res_arr[22] = res_arr[18]
+res_arr[31] = np.median(res_arr[[30, 29, 33]], axis=0)
+res_arr[32] = np.median(res_arr[[30, 29, 33]], axis=0)
+points = np.array([x, y]).T
 
-res_1d = griddata(points, res_arr[40], (grid_x, grid_y), method='cubic')
+grid_x, grid_y = np.meshgrid(m_obj.grid_east[0:-1], m_obj.grid_north[0:-1])
+
+#lin_x = np.linspace()
+#grid_x, grid_y = np.meshgrid()
+
+sm1d_res = np.zeros_like(m_obj.res_model)
+
+for ii in range(res_arr.shape[-1]):
+    res_1d = griddata(points,
+                      res_arr[:, ii], 
+                      (grid_x, grid_y),
+                      method='cubic')
+    sm1d_res[:, :, ii] = res_1d
+
+topo_index = np.where(m_obj.res_model > 1E10)
+sm1d_res[topo_index] = 12
+sm1d_res = np.nan_to_num(sm1d_res)
+sm1d_res[np.where(sm1d_res == 0.0)] = 2.0
+
+m_obj.write_model_file(save_path=r"c:\Users\jpeacock\Documents\MountainPass\modem_inv\inv_08",
+                       model_fn_basename="mp_sm1d_rough.rho",
+                       res_model=10**(sm1d_res))
 
 
 #res_1d = np.zeros_like(m_obj.res_model)
