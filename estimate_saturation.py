9671b8c133e19bde2b5e090c42ec1e953d882d27
JP <peacock.jared@gmail.com>
2019-08-02 17:11:36 -0700

    adding scripts

--- estimate_saturation.py
+++ estimate_saturation.py
@@ -7,6 +7,7 @@ Created on Fri Apr 26 16:44:42 2019
 import numpy as np
 from mtpy.modeling import modem
 from scipy import interpolate
+import matplotlib.pyplot as plt
 
 fn = r"c:\Users\jpeacock\Documents\Geysers\modem_inv\inv04\gz_rm50_z03_c05_107.rho"
 
@@ -18,26 +19,29 @@ def glover(sigma_1, phi_1, m_1, sigma_2, phi_2, m_2,
 
     return sigma_eff
 
-phi_3 = .04
-m_3 = 3
+phi_3 = .05
+m_3 = 3.6
 sigma_3 = 100
-saturation = np.linspace(phi_3, 1, 25)
-s = 1./glover(1./70, 1-saturation, 1,
-              1./1000, saturation, 2, 
+sigma_rock = 1./100
+saturation = np.linspace(phi_3, .95, 25)
+s = 1./glover(sigma_rock, .95-saturation, .5,
+              1./1000, saturation, 1.5, 
               sigma_3, phi_3, m_3)
 
-s_interp = interpolate.interp1d(s, saturation, kind='slinear',
-                                bounds_error=False, 
-                                fill_value=np.NAN,
-                                assume_sorted=False)
+plt.plot(saturation, s)
 
-m = modem.Model()
-m.read_model_file(fn)
-
-res = s_interp(m.res_model.copy().flatten()).reshape(m.res_model.shape)
-
-m.res_model = res.copy()
-m.write_model_file(model_fn_basename='gz_saturation.rho')
-m.write_vtk_file(vtk_fn_basename='gz_saturation')
+#s_interp = interpolate.interp1d(s, saturation, kind='slinear',
+#                                bounds_error=False, 
+#                                fill_value=np.NAN,
+#                                assume_sorted=False)
+#
+#m = modem.Model()
+#m.read_model_file(fn)
+#
+#res = s_interp(m.res_model.copy().flatten()).reshape(m.res_model.shape)
+#
+#m.res_model = res.copy()
+#m.write_model_file(model_fn_basename='gz_saturation.rho')
+#m.write_vtk_file(vtk_fn_basename='gz_saturation')
 
 
