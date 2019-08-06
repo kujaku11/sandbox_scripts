01ec2286f524f9d1d161e7d13f5782e7473aa91b
JP <peacock.jared@gmail.com>
2019-08-06 11:26:40 -0700

    adding scripts

---
+++ arcgis_convert_resistivity_to_points.py
@@ -0,0 +1,96 @@
+'''
+compare resistivity from 3D depth slices with geology
+'''
+
+##Import modules, initial settings
+import sys, string, os, time, csv, arcpy
+import numpy as np
+
+
+## Print start time
+starttime = time.clock()
+print("Starting  " + time.strftime("%I:%M:%S", time.localtime()))
+
+## Toolboxes ect...
+arcpy.gp.overwriteOutput = True
+arcpy.CheckOutExtension("spatial")
+arcpy.CheckOutExtension("GeoStats")
+
+# User Inputs:
+input_path = r'c:\Users\jpeacock\OneDrive - DOI\Geysers\depth_slices' # Folder with input data
+output_path = r'c:\Users\jpeacock\Documents\ClearLake\GIS\resistivity' # Folder where outputs will be located
+geology_shp = r'c:\Users\jpeacock\Documents\ClearLake\GIS\geology_units_slim.shp'
+
+rastersToSample = sorted([fn for fn in os.listdir(input_path) if fn.endswith('.tif')])
+sort_dict = {}
+for fn in rastersToSample:
+    find_01 = fn.find('_') + 1
+    if fn.count('m') > 1:
+        find_02 = fn.find('m', 4)
+        find_01 += 1
+        scale = -1
+    else:
+        find_02 = fn.find('m')
+        scale = 1
+    depth = int(fn[find_01:find_02]) * scale
+    sort_dict[depth] = fn
+
+raster_list = []
+for key in sorted(sort_dict.keys()):
+    raster_list.append(sort_dict[key])
+
+
+'''
+Setup
+'''
+
+studyAreaRasterName = raster_list[0] # Name of study area raster
+samplingPointsName = 'resistivity_sampling.shp'# Name of Sampling Points File
+
+# Set Geoprocessing environments: snap, extent, mask, cell size equal to study area raster
+studyAreaRaster = os.path.join(input_path, studyAreaRasterName)
+rastExtent = arcpy.sa.Raster(studyAreaRaster).extent
+arcpy.env.extent = rastExtent
+arcpy.env.snapRaster = studyAreaRaster
+arcpy.env.mask = studyAreaRaster
+pdCellSize = int(str(arcpy.GetRasterProperties_management(studyAreaRaster, "CELLSIZEX")))
+print('Cell Size = ' + str(pdCellSize))
+
+'''
+Geoprocessing
+'''
+
+# Create study area points from study area raster
+studyAreaPoints = os.path.join(output_path, samplingPointsName)
+arcpy.RasterToPoint_conversion(studyAreaRaster, studyAreaPoints, "Value")
+arcpy.DeleteField_management(studyAreaPoints, "GRID_CODE")
+arcpy.AddXY_management(studyAreaPoints)
+
+
+# Sample rasters to point file
+for rastLayer in raster_list[:]:
+    StudyAreaPointsTemp = os.path.join(output_path, 'tempPts1.shp')
+    evidenceRaster = os.path.join(input_path, rastLayer)
+    arcpy.gp.ExtractValuesToPoints_sa(studyAreaPoints, evidenceRaster, StudyAreaPointsTemp, "NONE", "VALUE_ONLY")
+    arcpy.AddField_management(StudyAreaPointsTemp, os.path.splitext(rastLayer)[0], "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+    arcpy.CalculateField_management(StudyAreaPointsTemp, os.path.splitext(rastLayer)[0], "!RASTERVALU!", "PYTHON", "")
+    arcpy.DeleteField_management(StudyAreaPointsTemp, "RASTERVALU")
+    arcpy.CopyFeatures_management(StudyAreaPointsTemp, studyAreaPoints, "", "0", "0", "0")
+    arcpy.Delete_management(StudyAreaPointsTemp, "")
+
+
+# Sample geology to point file
+StudyAreaPointsTemp = os.path.join(output_path, 'tempPts2.shp')
+arcpy.SpatialJoin_analysis(studyAreaPoints, geology_shp, StudyAreaPointsTemp, "#", "#")
+arcpy.CopyFeatures_management(StudyAreaPointsTemp, studyAreaPoints, "", "0", "0", "0")
+arcpy.Delete_management(StudyAreaPointsTemp, "")
+
+
+'''
+Finishing up, Clock
+'''
+stoptime = time.clock()
+print "Done  " + time.strftime("%I:%M:%S", time.localtime())
+elapsed = stoptime-starttime
+print "Elapsed time in seconds "
+print elapsed
--- modem_to_raster_geysers.py
+++ modem_to_raster_geysers.py
@@ -9,16 +9,16 @@ import mtpy.utils.array2raster as a2r
 import os
 
 #model_fn = r"c:\Users\jpeacock\Documents\Geothermal\Washington\MB\modem_inv\mb_rot_tip02_cov03_NLCG_083.rho"
-model_fn = r"c:\Users\jpeacock\Documents\ClearLake\modem_inv\inv04\gz_rm50_z03_c02_104.rho"
+model_fn = r"c:\Users\jpeacock\OneDrive - DOI\Geysers\gz_z03_c02_074.rho"
 
 
-save_path = r"c:\Users\jpeacock\Documents\ClearLake\modem_inv\inv04\depth_slices"
+save_path = r"c:\Users\jpeacock\OneDrive - DOI\Geysers\depth_slices"
 model_center = (-122.828190, 38.831979)
 
 def check_dir(directory_path):
     if os.path.isdir(directory_path) is False:
         os.mkdir(directory_path)
-        print 'Made directory {0}'.format(directory_path)
+        print('Made directory {0}'.format(directory_path))
         
 check_dir(save_path)
 
@@ -27,8 +27,8 @@ modem_raster.model_fn = model_fn
 ll_cc = modem_raster.get_model_lower_left_coord(model_center=model_center,
                                                 pad_east=5, pad_north=5)
 #modem_raster.lower_left_corner = (ll_cc[0]+0.01, ll_cc[1]-0.000)
-modem_raster.lower_left_corner = (ll_cc[0]+.164, ll_cc[1]-0.055)
+modem_raster.lower_left_corner = (ll_cc[0]+.015, ll_cc[1]-.014)
 modem_raster.save_path = save_path
 modem_raster.projection = 'WGS84'
-modem_raster.rotation_angle = 50
+modem_raster.rotation_angle = 0
 modem_raster.write_raster_files()
