cc194fcdfca6f0e73d749a4d0619e61060866d9e
JP <peacock.jared@gmail.com>
2020-04-08 17:35:58 -0700

    adding scripts

---
+++ birrp_processing_local_linux_mnp.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 26 12:32:29 2016
+
+@author: jpeacock
+"""
+
+import os
+import shutil
+import mtpy.usgs.zen_processing as zp
+
+#==============================================================================
+# local parameters
+#==============================================================================
+coil_calibration_path = r"/mnt/hgfs/MT/birrp_calibrations"
+birrp_path = r"/home/peacock/Documents/birrp52/SourceCode/birrp52_big"
+local_path = r"/mnt/hgfs/MT/MNP2019"
+copy_edi_path = os.path.join(local_path, 'EDI_Files_birrp')
+
+if not os.path.exists(copy_edi_path):
+    os.mkdir(copy_edi_path)
+
+#==============================================================================
+# Station to process and remote reference
+#==============================================================================
+station = 'mnp172'
+rr_station = ['mnp151', 'mnp162', 'mnp128']
+# rr_station = None
+
+block_dict = {4096:[0, 1, 2],
+              256:[0, 1, 2, 3],
+              4:[0]}
+
+use_df_list = [4096]
+overwrite = False
+
+
+local_station_path = os.path.join(local_path, station)
+if rr_station is not None:
+    if isinstance(rr_station, list):
+        rr_local_station_path = []
+        for rr in rr_station:
+            rr_local_station_path.append(os.path.join(local_path, rr))
+    else:
+        rr_local_station_path = os.path.join(local_path, rr_station)
+    ilev=1
+else:
+    rr_local_station_path = None
+    ilev = 0
+    
+#==============================================================================
+# Process data
+#==============================================================================
+
+b_param_dict = {'ilev': ilev,
+                'c2threshb': .35,
+                'c2threshe': .35,
+                'c2thresh1': .35,
+                'ainuin': .95,
+                'ainlin': .05,
+                'nar': 11,
+                'tbw': 2}
+
+kwargs_dict = {'calibration_list':['2254', '2264', '2274', '2284', '2294',
+                                '2304', '2314', '2324', '2334', '2344',
+                                '2844', '2854', '3214'],
+               '_max_nread':25000000}
+
+zp_obj = zp.Z3D2EDI(station_z3d_dir=local_station_path,
+                    rr_station_z3d_dir=rr_local_station_path,
+                    station_ts_dir=os.path.join(local_station_path, 'TS'))
+zp_obj.birrp_exe = birrp_path
+zp_obj.calibration_path = coil_calibration_path
+
+plot_obj, comb_edi_fn = zp_obj.process_data(df_list=use_df_list,
+                                            notch_dict={4096:None,
+                                                        256:None,
+                                                        4:None},
+                                            max_blocks=4,
+                                            sr_dict={4096:(1000., 25),
+                                                     256:(24.999, .1), 
+                                                     4:(.1, .00001)},
+                                            use_blocks_dict=block_dict,
+                                            birrp_param_dict=b_param_dict,
+                                            overwrite=overwrite,
+                                            **kwargs_dict)
+                                            
+#cp_edi_fn = os.path.join(copy_edi_path, station+'.edi')
+#shutil.copy(comb_edi_fn, cp_edi_fn)
+#print('--> Copied {0} to {1}'.format(comb_edi_fn, cp_edi_fn))
---
+++ read_amtant_cal_02.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 16:55:36 2020
+
+@author: peacock
+"""
+
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+ant_cal_fn = r"/mnt/hgfs/MT/amtant.cal"
+sv_path = Path(r"/mnt/hgfs/MT/birrp_calibrations")
+if not sv_path.exists():
+    sv_path.mkdir()
+
+fn_path = Path(ant_cal_fn)
+
+### open file
+with open(ant_cal_fn, 'r') as fid:
+    lines = fid.readlines()
+
+### sort data
+data = {}
+for line in lines:
+    line = line.strip()
+    if line.find('antenna') > 0:
+        freq = float(line.split()[-1])*2*np.pi
+    elif len(line) > 0:
+        ant, amp, phase, amp2, phase2 = [float(ii) for ii in line.split()]
+        if ant != 0:
+            z_real = amp * np.cos(phase/1000.)
+            z_imag = amp * np.sin(phase/1000.)
+            
+            z_real2 = amp * np.cos(phase/1000.) #
+            z_imag2 = amp * np.sin(phase/1000.) #
+            entry = [freq, amp, phase, amp2, phase2,
+                     z_real, z_imag, z_real2, z_imag2]
+            
+            if ant not in list(data.keys()):
+                data[int(ant)] = [[1]*9] # birrp wants first line to be a scale
+            data[ant].append(entry)
+            
+for key in list(data.keys()):
+    sv_fn = Path.joinpath(sv_path.parent, sv_path.name, '{0}.csv'.format(key))
+    df = pd.DataFrame(data[key],
+                      columns=['freq','amp','phase','amp2','phase2',
+                               'z_real', 'z_imag', 'z_real2', 'z_imag2'])
+    df.to_csv(sv_fn, columns=['freq', 'z_real', 'z_imag'],
+              index=False, header=False)
+            
+
+
+    
