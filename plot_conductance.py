ce120eb1b1a5b84162d8ceca67069adf66a1b292
JP <peacock.jared@gmail.com>
2021-03-31 13:42:21 -0700

    adding scripts

--- plot_conductance.py
+++ plot_conductance.py
@@ -13,12 +13,25 @@ from matplotlib import pyplot as plt
 from mtpy.modeling import modem
 from mtpy.utils import array2raster
 
-mfn = r"c:\Users\jpeacock\OneDrive - DOI\Geothermal\GabbsValley\modem_inv\st_topo_inv_02\st_z05_t02_c025_126.rho"
 
-z_dict = {"surface": np.array((0, 3000)),
-          "middle_crust": np.array((3000, 12000)),
-          "lower_crust": np.array((12000, 27000))}
-pad = 7
+dfn = r"c:\Users\jpeacock\OneDrive - DOI\Geothermal\Umatilla\modem_inv\inv_09\um_modem_data_z05_te2.dat"
+d = modem.Data()
+d.read_data_file(dfn)
+
+
+# mfn = r"c:\Users\jpeacock\OneDrive - DOI\Geothermal\GabbsValley\modem_inv\st_topo_inv_02\st_z05_t02_c025_126.rho"
+mfn = r"c:\Users\jpeacock\OneDrive - DOI\Geothermal\Umatilla\modem_inv\inv_09\um_z05_c05_089.rho"
+# mfn = r"c:\Users\jpeacock\OneDrive - DOI\Geothermal\Umatilla\modem_inv\inv_06\um_z05_c03_083.rho"
+# model_center = (-118.704435 + .03, 45.597757000000001 + .02)
+
+model_center = (-118.547148, 45.654713)
+
+z_dict = {"surface": np.array((-300, 700)),
+          "middle_crust": np.array((700, 4500)),
+          "lower_crust": np.array((4500, 30000))}
+pad = 9
+
+
 
 m = modem.Model()
 m.read_model_file(mfn)
@@ -41,11 +54,13 @@ for ii, key in enumerate(["surface", "middle_crust", "lower_crust"]):
                        cmap="gnuplot2", 
                        vmin=conductance[pad:-pad, pad:-pad].min(),
                        vmax=conductance[pad:-pad, pad:-pad].max())
+    
+    ax.scatter(d.station_locations.rel_east, d.station_locations.rel_north, marker='v', s=20)
     print(conductance[pad:-pad, pad:-pad].min(), conductance[pad:-pad, pad:-pad].max())
     ax.set_xlim((m.grid_east[pad], m.grid_east[-pad]))
     ax.set_ylim((m.grid_north[pad], m.grid_north[-pad]))
     
-    cb = plt.colorbar(im, ax=ax)
+    cb = plt.colorbar(im, ax=ax, shrink=.35)
     
 
     
@@ -53,6 +68,12 @@ for ii, key in enumerate(["surface", "middle_crust", "lower_crust"]):
     #                           (-118.590929, 38.541344000000002),
     #                           750.,
     #                           750.,
+    #                           conductance[pad:-pad, pad:-pad])
+    
+    # array2raster.array2raster(r"c:\Users\jpeacock\OneDrive - DOI\Geothermal\Umatilla\modem_inv\inv_09\conductance_{0}_crust.tiff".format(key),
+    #                           model_center,
+    #                           166.,
+    #                           110.,
     #                           conductance[pad:-pad, pad:-pad])
     
 plt.show()
