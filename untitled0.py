67ec506f4c4d37a869ca20bfacfce5d4748ac44e
JP <peacock.jared@gmail.com>
2021-08-18 12:56:06 -0700

    adding scripts

--- untitled0.py
+++ untitled0.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 28 10:01:20 2019
 
-@author: jpeacock
+Created on Tue Jun  1 12:28:28 2021
+
+:author: Jared Peacock
+
+:license: MIT
+
 """
+from pykml import parser
 
-import numpy as np
-import pymc3 as pm
-
-n= 500
-c1 = np.random.lognormal(10E-8, .01, n)
-c2 = np.random.lognormal(50, 5, n)
-
-s1 = .85
-s2 = 1 - s1
-
-m1 = .05
-m2 = 2
+kml_fn = r"c:\Users\peaco\Documents\FieldWork\ClearLake.kml"
+with open(kml_fn, 'r') as fid:
+    k = parser.parse(fid)
