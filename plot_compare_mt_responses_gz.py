99a1f8793acf0384bc64512b0806bde46d3a6068
JP <peacock.jared@gmail.com>
2021-09-17 15:42:15 -0700

    adding scripts

---
+++ plot_compare_mt_responses_gz.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 17:14:04 2017
+
+@author: jpeacock
+"""
+from pathlib import Path
+import numpy as np
+import mtpy.imaging.mtplot as mtplot
+
+original_dir = Path(r"c:\Users\jpeacock\OneDrive - DOI\Geysers\EDI_Files_birrp\Edited\Geographic")
+phase1_dir = Path(r"c:\Users\jpeacock\OneDrive - DOI\Geysers\CEC\EDI_files_birrp_processed\Geographic\Edited")
+save_dir = Path(r"c:\Users\jpeacock\OneDrive - DOI\Geysers\CEC\Reports")
+
+f1 =[]
+f2 = []
+for fn1 in original_dir.glob("*.edi"):
+    for fn2 in phase1_dir.glob("*.edi"):
+        if f"3{fn1.stem[-2:]}" == fn2.stem[2:]:
+            f1.append(fn1)
+            f2.append(fn2)
+            # pmr = mtplot.plot_multiple_mt_responses(
+            #     fn_list=[fn1, fn2], plot_style="compare", plot_pt="y"
+            # )
+            # pmr.fig.savefig(
+            #     save_dir.joinpath(f"{fn1.stem}_compare.png"), dpi=300,
+            # )
+            # mtplot.plotnresponses.plt.close("all")
+            break
+
+rpt = mtplot.plot_residual_pt_maps(f1, f2, frequencies=np.logspace(-3, 3, 40))
---
+++ plot_spectra_for_run.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 14 13:42:35 2021
+
+@author: jpeacock
+"""
+
+from pathlib import Path
+from scipy import signal
+from matplotlib import pyplot as plt
+from mtpy.usgs import zen
+
+save_path = Path(r"c:\Users\jpeacock\OneDrive - DOI\Geysers\CEC\Reports")
+# fn_stem = r"d:\WD SmartWare.swstor\IGSWMBWGLTGG032\Volume.b5634234.da89.11e2.aa2b.806e6f6e6963\MT\GZ2021\gz307\gz307_20210414_214517_256_"
+fn_stem = r"d:\WD SmartWare.swstor\IGSWMBWGLTGG032\Volume.b5634234.da89.11e2.aa2b.806e6f6e6963\MT\GZ2021\gz331\gz331_20210415_131018_256_"
+# fn_stem = r"d:\WD SmartWare.swstor\IGSWMBWGLTGG032\Volume.b5634234.da89.11e2.aa2b.806e6f6e6963\MT\GZ2021\gz330\gz330_20210416_010018_4096_"
+
+comps = ["ex", "ey", "hx", "hy"]
+colors = [(28/255, 146/255, 205/255), 
+          (28/255, 52/255, 205/255),
+          (203/255, 158/255, 33/255),
+          (203/255, 61/255, 33/255)]
+fig = plt.figure(1, dpi=150)
+fig.clf()
+ax = fig.add_subplot(1, 1, 1)
+p_dict = {"fs": 256, "nperseg": 2**13}
+line_list = []
+for ii, comp, c in zip(range(len(comps)), comps, colors):
+    fn = Path(f"{fn_stem}{comp.upper()}.Z3D")
+    z1 = zen.Zen3D(fn)
+    z1.read_z3d()
+    z1.apply_adaptive_notch_filter()
+    
+    b = z1.plot_spectrogram(plot_type='all')
+    b.save_figure(save_path.joinpath(f"{fn.stem}.png").as_posix(), fig_dpi=150)
+    
+    f, p = signal.welch(z1.ts_obj.ts.data, **p_dict)
+    l1, = ax.loglog(f, p, lw=1.75, color=c)
+    l1.label = comp.capitalize()
+    line_list.append(l1)
+
+    
+ax.set_xlabel("Frequency (Hz)", fontdict={"size": 10, "weight": "bold"})
+ax.set_ylabel("Power (dB)", fontdict={"size": 10, "weight": "bold"})
+ax.axis("tight")
+ax.grid(which="both")
+ax.legend(line_list, comps, loc="upper right")
+
+
+plt.show()
+fig.savefig(save_path.joinpath(f"{fn.stem[0:5]}_256_spectra.png"), dpi=300)    
+    
+    
